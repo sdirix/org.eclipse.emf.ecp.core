79b4bd80d1dd0cd79da0c8066e7d42897c2751b7
Florian Pirchner <florian.pirchner@gmail.com>
2012-09-20 18:22:07 +0200

    Removed a warning in javadoc.

--- ECP2/org.eclipse.emf.ecp.core/src/org/eclipse/emf/ecp/spi/core/DefaultProvider.java
+++ ECP2/org.eclipse.emf.ecp.core/src/org/eclipse/emf/ecp/spi/core/DefaultProvider.java
@@ -353,6 +353,7 @@ public abstract class DefaultProvider extends Element implements InternalProvide
    * Convenient implementation of the {@link #getUnsupportedEPackages(Collection)} method to return an empty list. The
    * provider has to {@link Override} this method if not all {@link EPackage}s are supported.
    */
+  @SuppressWarnings("javadoc")
   public Collection<EPackage> getUnsupportedEPackages(Collection<EPackage> packages, InternalRepository repository)
   {
     return Collections.emptyList();
