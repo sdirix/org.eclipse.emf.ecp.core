8ee32d75e4ae743b1db311edfc12c72adb31a981
eneufeld <neufeld.eugen@googlemail.com>
2013-01-12 11:47:37 +0100

    applied patch 397976

---
+++ ECP2/org.eclipse.emf.ecp.emfstore.ui/src/org/eclipse/emf/ecp/emfstore/internal/ui/property/EMFStoreProjectCanUndoTester.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.eclipse.emf.ecp.emfstore.internal.ui.property;
+
+import org.eclipse.emf.ecp.emfstore.core.internal.EMFStoreProvider;
+import org.eclipse.emf.ecp.spi.core.InternalProject;
+import org.eclipse.emf.emfstore.client.model.ProjectSpace;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+/**
+ * @author Tobias Verhoeven
+ * 
+ */
+public class EMFStoreProjectCanUndoTester extends PropertyTester {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.expressions.IPropertyTester#test(java.lang.Object, java.lang.String, java.lang.Object[],
+	 * java.lang.Object)
+	 */
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		ProjectSpace ps = EMFStoreProvider.INSTANCE.getProjectSpace((InternalProject) receiver);
+		Boolean result = Boolean.valueOf(ps.isDirty());
+		return result;
+	}
+}
