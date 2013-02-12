d349f7d0ce210849579bfde1c1db35469afd2c61
eneufeld <neufeld.eugen@googlemail.com>
2013-02-12 18:24:19 +0100

    updated javadoc

--- ECP2/org.eclipse.emf.ecp.workspace.ui/src/org/eclipse/emf/ecp/workspace/internal/ui/Activator.java
+++ ECP2/org.eclipse.emf.ecp.workspace.ui/src/org/eclipse/emf/ecp/workspace/internal/ui/Activator.java
@@ -4,9 +4,8 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
  * Contributors:
- *    Eike Stepper - initial API and implementation
+ * Eike Stepper - initial API and implementation
  */
 package org.eclipse.emf.ecp.workspace.internal.ui;
 
@@ -19,73 +18,93 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
+ * The activator class controls the plug-in life cycle.
+ * 
  * @author Eike Stepper
  */
-public final class Activator extends AbstractUIPlugin
-{
-  public static final String PLUGIN_ID = "org.eclipse.emf.ecp.workspace.ui"; //$NON-NLS-1$
+public final class Activator extends AbstractUIPlugin {
+	/**
+	 * The plug-in ID.
+	 */
+	public static final String PLUGIN_ID = "org.eclipse.emf.ecp.workspace.ui"; //$NON-NLS-1$
 
-  private static Activator instance;
+	private static Activator instance;
 
-  public Activator()
-  {
-  }
+	/**
+	 * The constructor.
+	 */
+	public Activator() {
+	}
 
-  @Override
-  public void start(BundleContext context) throws Exception
-  {
-    super.start(context);
-    instance = this;
-  }
+	// BEGIN SUPRESS CATCH EXCEPTION
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		instance = this;
+	}
 
-  @Override
-  public void stop(BundleContext context) throws Exception
-  {
-    instance = null;
-    super.stop(context);
-  }
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		instance = null;
+		super.stop(context);
+	}
 
-  public static Activator getInstance()
-  {
-    return instance;
-  }
+	// END SUPRESS CATCH EXCEPTION
+	/**
+	 * Returns the shared instance.
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getInstance() {
+		return instance;
+	}
 
-  public static void log(String message)
-  {
-    instance.getLog().log(new Status(IStatus.INFO, PLUGIN_ID, message));
-  }
+	/**
+	 * Logs and Info message.
+	 * 
+	 * @param message the message to log
+	 */
+	public static void log(String message) {
+		instance.getLog().log(new Status(IStatus.INFO, PLUGIN_ID, message));
+	}
 
-  public static void log(IStatus status)
-  {
-    instance.getLog().log(status);
-  }
+	private static void log(IStatus status) {
+		instance.getLog().log(status);
+	}
 
-  public static String log(Throwable t)
-  {
-    IStatus status = getStatus(t);
-    log(status);
-    return status.getMessage();
-  }
+	/**
+	 * Logs a {@link Throwable}.
+	 * 
+	 * @param t the {@link Throwable} to log
+	 * @return the message of the {@link Throwable}
+	 */
+	public static String log(Throwable t) {
+		IStatus status = getStatus(t);
+		log(status);
+		return status.getMessage();
+	}
 
-  public static IStatus getStatus(Throwable t)
-  {
-    if (t instanceof CoreException)
-    {
-      CoreException coreException = (CoreException)t;
-      return coreException.getStatus();
-    }
+	private static IStatus getStatus(Throwable t) {
+		if (t instanceof CoreException) {
+			CoreException coreException = (CoreException) t;
+			return coreException.getStatus();
+		}
 
-    String msg = t.getLocalizedMessage();
-    if (msg == null || msg.length() == 0)
-    {
-      msg = t.getClass().getName();
-    }
+		String msg = t.getLocalizedMessage();
+		if (msg == null || msg.length() == 0) {
+			msg = t.getClass().getName();
+		}
 
-    return new Status(IStatus.ERROR, PLUGIN_ID, msg, t);
-  }
+		return new Status(IStatus.ERROR, PLUGIN_ID, msg, t);
+	}
 
-  public static ImageDescriptor loadImageDescriptor(String path)
-  {
-    return imageDescriptorFromPlugin(PLUGIN_ID, path);
-  }
+	/**
+	 * Returns an {@link ImageDescriptor} for a path.
+	 * 
+	 * @param path the path to an image
+	 * @return the {@link ImageDescriptor}
+	 */
+	public static ImageDescriptor loadImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
 }
