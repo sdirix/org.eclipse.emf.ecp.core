42911fa780d80eb951e4b014e318f904d7af988e
eneufeld <neufeld.eugen@googlemail.com>
2013-03-13 17:58:44 +0100

    added Link as CellEditor, link doesn't do anything yet

---
+++ ECP2/org.eclipse.emf.ecp.edit.swt/src/org/eclipse/emf/ecp/edit/internal/swt/table/LinkCellEditorTester.java
@@ -0,0 +1,23 @@
+package org.eclipse.emf.ecp.edit.internal.swt.table;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecp.editor.util.ECPApplicableTester;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+
+public class LinkCellEditorTester implements ECPApplicableTester {
+
+	public LinkCellEditorTester() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int isApplicable(IItemPropertyDescriptor itemPropertyDescriptor, EObject eObject) {
+		EStructuralFeature feature = (EStructuralFeature) itemPropertyDescriptor.getFeature(null);
+		if (EReference.class.isInstance(feature)) {
+			return 1;
+		}
+		return NOT_APPLICABLE;
+	}
+
+}
