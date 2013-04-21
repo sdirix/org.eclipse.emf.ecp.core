bfa4784abc9ef2b51e101a3aab5275c84b2d6e0e
eneufeld <neufeld.eugen@googlemail.com>
2013-04-21 16:23:32 +0200

    added diagram persistency

--- bundles/org.eclipse.emf.ecp.graphiti.integration/src/org/eclipse/emf/ecp/graphiti/internal/integration/GenericECPGraphitiDiagramEditor.java
+++ bundles/org.eclipse.emf.ecp.graphiti.integration/src/org/eclipse/emf/ecp/graphiti/internal/integration/GenericECPGraphitiDiagramEditor.java
@@ -1,5 +1,6 @@
 package org.eclipse.emf.ecp.graphiti.internal.integration;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.ecp.graphiti.ECPGraphitiDiagramEditor;
 
 public class GenericECPGraphitiDiagramEditor extends ECPGraphitiDiagramEditor {
@@ -7,6 +8,11 @@ public class GenericECPGraphitiDiagramEditor extends ECPGraphitiDiagramEditor {
 	@Override
 	public boolean isDirty() {
 		return false;
+	}
+	@Override
+	public void dispose() {
+		doSave(new NullProgressMonitor());
+		super.dispose();
 	}
 
 }
--- bundles/org.eclipse.emf.ecp.graphiti.integration/src/org/eclipse/emf/ecp/graphiti/internal/integration/OpenGraphitiEditorHandler.java
+++ bundles/org.eclipse.emf.ecp.graphiti.integration/src/org/eclipse/emf/ecp/graphiti/internal/integration/OpenGraphitiEditorHandler.java
@@ -1,5 +1,8 @@
 package org.eclipse.emf.ecp.graphiti.internal.integration;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -12,6 +15,7 @@ import org.eclipse.emf.ecp.core.ECPProject;
 import org.eclipse.emf.ecp.graphiti.GraphitiDiagramEditorInput;
 import org.eclipse.emf.ecp.internal.graphiti.feature.LoadProjectFeature;
 import org.eclipse.emf.ecp.spi.core.InternalProject;
+import org.eclipse.emf.edit.command.ChangeCommand;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.graphiti.dt.IDiagramTypeProvider;
@@ -19,6 +23,7 @@ import org.eclipse.graphiti.features.context.impl.AddContext;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.services.GraphitiUi;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
@@ -34,66 +39,94 @@ public class OpenGraphitiEditorHandler extends AbstractHandler {
 		Object selObject = selection.getFirstElement();
 		if (!ECPProject.class.isInstance(selObject))
 			return null;
-		InternalProject selectedProject=(InternalProject) selObject;
-		Notifier projectRoot=selectedProject.getProvider().getRoot(selectedProject);
-		if(!EObject.class.isInstance(projectRoot)){
+		final InternalProject selectedProject = (InternalProject) selObject;
+		Notifier projectRoot = selectedProject.getProvider().getRoot(
+				selectedProject);
+		if (!EObject.class.isInstance(projectRoot)) {
 			return null;
 		}
-		EObject selectedEObject=(EObject) projectRoot;
-		TransactionalEditingDomain editingDomain = (TransactionalEditingDomain) selectedProject.getEditingDomain();
+		final EObject selectedEObject = (EObject) projectRoot;
+		TransactionalEditingDomain editingDomain = (TransactionalEditingDomain) selectedProject
+				.getEditingDomain();
 
-		ResourceSet resourceSet = editingDomain.getResourceSet();
+		final ResourceSet resourceSet = editingDomain.getResourceSet();
+		URI projectURI = selectedEObject.eResource().getURI();
+
+		String fileString = projectURI.toFileString();
+
+		fileString = fileString.substring(0, fileString.length()
+				- projectURI.fileExtension().length() - 1);
+		fileString += "_Diagram." + projectURI.fileExtension();
+
 		// create temporal resource
-		final Resource resource = resourceSet.createResource(URI.createURI(
-				"VIRTUAL_URI", false));
-
-		String diagramText=selectedProject.getName();
-		
-		final Diagram createDiagram = Graphiti.getPeCreateService()
-				.createDiagram("org.eclipse.emf.ecp.graphiti.diagramType",
-						diagramText, true);
-		createDiagram.setName(diagramText);
-		// add the diagram to the resource
-		editingDomain.getCommandStack().execute(
-				new RecordingCommand(editingDomain) {
-
-					@Override
-					protected void doExecute() {
-						resource.getContents().add(createDiagram);
-					}
-				});
-
-
-		
-		
-		GraphitiDiagramEditorInput input = new GraphitiDiagramEditorInput(createDiagram,selectedEObject
-				);
-		
+		// final Resource resource = resourceSet.createResource(URI..createURI(
+		// "VIRTUAL_URI", false));
+		final URI diagramUri = URI.createFileURI(fileString);
+		File diagramFile=new File(diagramUri.toFileString());
+		boolean loadExisting=true;
+		if (diagramFile.exists())
+			loadExisting=MessageDialog.openQuestion(HandlerUtil.getActiveShell(event), "Regenerate Diagram", "A diagram file already exists. Should the existing file be loaded?");
+		Diagram createDiagram=getDiagram(selectedProject, editingDomain, resourceSet, diagramUri,loadExisting);
+	
+	
+		GraphitiDiagramEditorInput input = new GraphitiDiagramEditorInput(
+				createDiagram, selectedEObject);
 		try {
 			// Needs IEditorInput (GraphitiDiagramEditorInput) for "openEditor"
-			GenericECPGraphitiDiagramEditor editor=(GenericECPGraphitiDiagramEditor) PlatformUI
+			GenericECPGraphitiDiagramEditor editor = (GenericECPGraphitiDiagramEditor) PlatformUI
 					.getWorkbench()
 					.getActiveWorkbenchWindow()
 					.getActivePage()
 					.openEditor(input,
 							GenericECPGraphitiDiagramEditor.EDITOR_ID, true);
-			
+
 			String providerId = GraphitiUi.getExtensionManager()
 					.getDiagramTypeProviderId(createDiagram.getDiagramTypeId());
 			final IDiagramTypeProvider dtp = GraphitiUi.getExtensionManager()
 					.createDiagramTypeProvider(createDiagram, providerId);
-			
-			AddContext context=new AddContext();
-			
+
+			AddContext context = new AddContext();
+
 			context.setTargetContainer(createDiagram);
 			
-			editor.executeFeature(new LoadProjectFeature(dtp.getFeatureProvider(),selectedProject), context);
+				if(!loadExisting)
+				editor.executeFeature(
+						new LoadProjectFeature(dtp.getFeatureProvider(),
+								selectedProject), context);
 			
+
 		} catch (PartInitException e) {
 			System.out.println("Error");
-
+			e.printStackTrace();
 		}
 		return null;
 	}
 
+	private Diagram getDiagram(ECPProject selectedProject,
+			TransactionalEditingDomain editingDomain,
+			final ResourceSet resourceSet, final URI diagramUri,boolean loadExisting) {
+		if (!loadExisting) {
+			String diagramText = selectedProject.getName();
+
+			final Diagram createDiagram = Graphiti.getPeCreateService()
+					.createDiagram("org.eclipse.emf.ecp.graphiti.diagramType",
+							diagramText, true);
+			createDiagram.setName(diagramText);
+
+			// add the diagram to the resource
+
+			editingDomain.getCommandStack().execute(
+					new RecordingCommand(editingDomain) {
+
+						@Override
+						protected void doExecute() {
+							resourceSet.createResource(diagramUri)
+									.getContents().add(createDiagram);
+						}
+					});
+			return createDiagram;
+		}
+		return (Diagram) resourceSet.getResource(diagramUri, true)
+				.getContents().get(0);
+	}
 }
