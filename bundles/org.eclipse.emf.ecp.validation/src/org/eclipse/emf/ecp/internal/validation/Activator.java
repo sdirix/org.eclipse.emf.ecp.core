72977ffeb1bb292b590ec3a06b0c41d2f82e1a9a
eneufeld <neufeld.eugen@googlemail.com>
2013-05-06 11:15:35 +0200

    updated validation to use plugin instead of uiplugin

--- bundles/org.eclipse.emf.ecp.validation/src/org/eclipse/emf/ecp/internal/validation/Activator.java
+++ bundles/org.eclipse.emf.ecp.validation/src/org/eclipse/emf/ecp/internal/validation/Activator.java
@@ -10,13 +10,13 @@
  ******************************************************************************/
 package org.eclipse.emf.ecp.internal.validation;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle.
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends Plugin {
 
 	/**
 	 * The plug-in ID.
