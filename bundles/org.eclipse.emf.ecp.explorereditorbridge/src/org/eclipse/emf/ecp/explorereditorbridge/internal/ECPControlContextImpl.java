6ad677eb01ff1d114b048799633f662761f95a7d
eneufeld <neufeld.eugen@googlemail.com>
2013-05-16 20:21:54 +0200

    added locale support

--- bundles/org.eclipse.emf.ecp.explorereditorbridge/src/org/eclipse/emf/ecp/explorereditorbridge/internal/ECPControlContextImpl.java
+++ bundles/org.eclipse.emf.ecp.explorereditorbridge/src/org/eclipse/emf/ecp/explorereditorbridge/internal/ECPControlContextImpl.java
@@ -42,6 +42,7 @@ import org.eclipse.ui.PlatformUI;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.Set;
 
 /**
@@ -303,6 +304,10 @@ public class ECPControlContextImpl implements ECPControlContext {
 	public boolean isRunningAsWebApplication() {
 		// TODO IMPLEMENT to be generic
 		return false;
+	}
+
+	public Locale getLocale() {
+		return Locale.getDefault();
 	}
 
 }
