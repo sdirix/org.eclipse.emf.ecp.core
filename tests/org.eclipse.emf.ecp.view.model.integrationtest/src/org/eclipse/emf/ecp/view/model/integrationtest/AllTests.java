3c1c99ab8ffb76c18a26e9d74c37dd0108260cb3
jonas <jonas.helming@googlemail.com>
2013-07-30 13:48:49 +0200

    Bug 413416 - [ViewModel] Add support for custom control - fixed visibility of child descriptors, added Integration Tests

---
+++ tests/org.eclipse.emf.ecp.view.model.integrationtest/src/org/eclipse/emf/ecp/view/model/integrationtest/AllTests.java
@@ -0,0 +1,11 @@
+package org.eclipse.emf.ecp.view.model.integrationtest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ChildrenDescriptorExtensionTest.class })
+public class AllTests {
+
+}
---
+++ tests/org.eclipse.emf.ecp.view.model.integrationtest/src/org/eclipse/emf/ecp/view/model/integrationtest/ChildrenDescriptorExtensionTest.java
@@ -0,0 +1,214 @@
+package org.eclipse.emf.ecp.view.model.integrationtest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecp.view.model.ViewPackage;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+
+import org.junit.Test;
+
+public class ChildrenDescriptorExtensionTest {
+
+	/**
+	 * Needs to be adapted after refactoring
+	 */
+	private static final int VIEW_CHILD_COUNT = 7;
+	private static final int SHOWRULE_CHILD_COUNT = 3;
+	private static final int ENABLERULE_CHILD_COUNT = 3;
+	private static final int LEAFCONDITION_CHILD_COUNT = 0;
+	private static final int ORCONDITION_CHILD_COUNT = 3;
+	private static final int ANDCONDITION_CHILD_COUNT = 3;
+	private static final int CATEGORIZATION_CHILD_COUNT = 7;
+	private static final int CATEGORY_CHILD_COUNT = 11;
+	private static final int CONTROL_CHILD_COUNT = 2;
+	private static final int TABLECONTROL_CHILD_COUNT = 3;
+	private static final int TABLECOLUMN_CHILD_COUNT = 0;
+	private static final int CUSTOMCOMPOSITE_CHILD_COUNT = 2;
+	private static final int SEPARATOR_CHILD_COUNT = 2;
+	private static final int COLUMNCOMPOSITE_CHILD_COUNT = 10;
+	private static final int COLUMN_CHILD_COUNT = 10;
+	private static final int GROUP_CHILD_COUNT = 10;
+	private static final int TREECATEGORY_CHILD_COUNT = 11;
+	private static final int ACTION_CHILD_COUNT = 0;
+	private AdapterFactoryEditingDomain domain = new AdapterFactoryEditingDomain(new ComposedAdapterFactory(
+		ComposedAdapterFactory.Descriptor.Registry.INSTANCE), new BasicCommandStack());
+
+	@Test
+	public void testViewChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getView());
+		assertEquals(VIEW_CHILD_COUNT, size);
+	}
+
+	/**
+	 * Class is abstract, Exception expected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testAbstractCategorizationChildDescriptors() {
+		getChildrenSize(ViewPackage.eINSTANCE.getAbstractCategorization());
+	}
+
+	/**
+	 * Class is abstract, Exception expected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testRuleChildDescriptors() {
+		getChildrenSize(ViewPackage.eINSTANCE.getRule());
+	}
+
+	@Test
+	public void testShowRuleChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getShowRule());
+		assertEquals(SHOWRULE_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testEnableRuleChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getEnableRule());
+		assertEquals(ENABLERULE_CHILD_COUNT, size);
+	}
+
+	/**
+	 * Class is abstract, Exception expected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testConditionChildDescriptors() {
+		getChildrenSize(ViewPackage.eINSTANCE.getCondition());
+	}
+
+	@Test
+	public void testLeafConditionChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getLeafCondition());
+		assertEquals(LEAFCONDITION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testOrConditionChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getOrCondition());
+		assertEquals(ORCONDITION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testAndConditionChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getAndCondition());
+		assertEquals(ANDCONDITION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testCategorizationChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getCategorization());
+		assertEquals(CATEGORIZATION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testCategoryChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getCategory());
+		assertEquals(CATEGORY_CHILD_COUNT, size);
+	}
+
+	/**
+	 * Class is abstract, Exception expected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCompositeChildDescriptors() {
+		getChildrenSize(ViewPackage.eINSTANCE.getComposite());
+	}
+
+	@Test
+	public void testControlChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getControl());
+		assertEquals(CONTROL_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testTableControlChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getTableControl());
+		assertEquals(TABLECONTROL_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testTableColumnChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getTableColumn());
+		assertEquals(TABLECOLUMN_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testCustomCompositeColumnChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getCustomComposite());
+		assertEquals(CUSTOMCOMPOSITE_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testSeparatorChildDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getSeperator());
+		assertEquals(SEPARATOR_CHILD_COUNT, size);
+	}
+
+	/**
+	 * Class is abstract, Exception expected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testCompositeCollectionDescriptors() {
+		getChildrenSize(ViewPackage.eINSTANCE.getCompositeCollection());
+	}
+
+	@Test
+	public void testColumnCompositeDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getColumnComposite());
+		assertEquals(COLUMNCOMPOSITE_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testColumnDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getColumn());
+		assertEquals(COLUMN_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testGroupDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getGroup());
+		assertEquals(GROUP_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testTreeCategoryDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getTreeCategory());
+		assertEquals(TREECATEGORY_CHILD_COUNT, size);
+	}
+
+	/**
+	 * Class is abstract, Exception expected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testRenderableDescriptors() {
+		getChildrenSize(ViewPackage.eINSTANCE.getRenderable());
+	}
+
+	@Test
+	public void testActionDescriptors() {
+		int size = getChildrenSize(ViewPackage.eINSTANCE.getAction());
+		assertEquals(ACTION_CHILD_COUNT, size);
+	}
+
+	/**
+	 * Class is abstract, Exception expected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testAbstractControlDescriptors() {
+		getChildrenSize(ViewPackage.eINSTANCE.getAbstractControl());
+	}
+
+	/**
+	 * @param category
+	 * @return
+	 */
+	private int getChildrenSize(EClass eClass) {
+		EObject eObject = EcoreUtil.create(eClass);
+		return domain.getNewChildDescriptors(eObject, null).size();
+	}
+
+}
