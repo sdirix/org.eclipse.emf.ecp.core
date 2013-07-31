c4e621204d7e09aefaf64ddc9a103bb5640313b0
jonas <jonas.helming@googlemail.com>
2013-07-31 15:51:34 +0200

    Bug 414041 - [View Model] Extract Group in own model, added Test Cases

---
+++ tests/org.eclipse.emf.ecp.view.group.ui.swt.test/src/org/eclipse/emf/ecp/view/group/ui/swt/test/SWTGroupTest.java
@@ -0,0 +1,162 @@
+package org.eclipse.emf.ecp.view.group.ui.swt.test;
+
+/*******************************************************************************
+ * Copyright (c) 2011-2013 EclipseSource Muenchen GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Johannes Faltermeier
+ * 
+ *******************************************************************************/
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.eclipse.emf.ecp.internal.ui.view.renderer.NoPropertyDescriptorFoundExeption;
+import org.eclipse.emf.ecp.internal.ui.view.renderer.NoRendererFoundException;
+import org.eclipse.emf.ecp.view.group.ui.test.GroupTest;
+import org.eclipse.emf.ecp.view.model.View;
+import org.eclipse.emf.ecp.view.test.common.swt.DatabindingClassRunner;
+import org.eclipse.emf.ecp.view.test.common.swt.SWTViewTestHelper;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(DatabindingClassRunner.class)
+public class SWTGroupTest {
+
+	private static final String GROUP_NAME = "group";
+	private static final String GROUP_NAME2 = "group2";
+
+	/**
+	 * 
+	 */
+
+	@Test
+	public void testOneGroupinView() throws NoRendererFoundException, NoPropertyDescriptorFoundExeption {
+
+		// setup model
+		final View view = GroupTest.createViewWithOneGroup();
+		final org.eclipse.emf.ecp.view.model.Group group = (org.eclipse.emf.ecp.view.model.Group) view.getChildren()
+			.get(0);
+		group.setName(GROUP_NAME);
+
+		// setup ui
+		final Shell shell = SWTViewTestHelper.createShell();
+
+		final Control control = SWTViewTestHelper.render(view, shell);
+		final Composite viewComposite = (Composite) shell.getChildren()[0];
+		final Control renderedControl = viewComposite.getChildren()[0];
+		assertTrue("Rendered Control is not a Group", renderedControl instanceof org.eclipse.swt.widgets.Group);
+		assertEquals(
+			"Rendered Control and control returned by renderer are not the same",
+			control, viewComposite);
+		final Group groupControl = (Group) renderedControl;
+		assertEquals("Rendered Group does not have correct name",
+			GROUP_NAME, groupControl.getText());
+		assertEquals("org_eclipse_emf_ecp_ui_group",
+			renderedControl.getData("org.eclipse.rap.rwt.customVariant"));
+	}
+
+	@Test
+	public void testTwoGroupsinView() throws NoRendererFoundException, NoPropertyDescriptorFoundExeption {
+
+		// setup model
+		final View view = GroupTest.createViewWithTwoGroups();
+		final org.eclipse.emf.ecp.view.model.Group group = (org.eclipse.emf.ecp.view.model.Group) view.getChildren()
+			.get(0);
+		group.setName(GROUP_NAME);
+
+		final org.eclipse.emf.ecp.view.model.Group group2 = (org.eclipse.emf.ecp.view.model.Group) view.getChildren()
+			.get(1);
+		group2.setName(GROUP_NAME2);
+
+		// setup ui
+		final Shell shell = SWTViewTestHelper.createShell();
+
+		final Control control = SWTViewTestHelper.render(view, shell);
+		final Composite viewComposite = (Composite) shell.getChildren()[0];
+		final Control renderedControl1 = viewComposite.getChildren()[0];
+		final Control renderedControl2 = viewComposite.getChildren()[1];
+		assertTrue("Rendered Control is not a Group", renderedControl1 instanceof org.eclipse.swt.widgets.Group);
+		assertEquals(
+			"Rendered Control and control returned by renderer are not the same",
+			control, viewComposite);
+		final Group groupControl1 = (Group) renderedControl1;
+		assertEquals("Rendered Group does not have correct name",
+			GROUP_NAME, groupControl1.getText());
+		assertEquals("org_eclipse_emf_ecp_ui_group",
+			renderedControl1.getData("org.eclipse.rap.rwt.customVariant"));
+
+		assertTrue("Rendered Control is not a Group", renderedControl2 instanceof org.eclipse.swt.widgets.Group);
+
+		final Group groupControl2 = (Group) renderedControl2;
+		assertEquals("Rendered Group does not have correct name",
+			GROUP_NAME2, groupControl2.getText());
+		assertEquals("org_eclipse_emf_ecp_ui_group",
+			renderedControl2.getData("org.eclipse.rap.rwt.customVariant"));
+	}
+
+	@Test
+	public void testTwoGroupsHierachicalinView() throws NoRendererFoundException, NoPropertyDescriptorFoundExeption {
+
+		final View view = GroupTest.createViewWithTwoHierachicalGroups();
+		final org.eclipse.emf.ecp.view.model.Group group = (org.eclipse.emf.ecp.view.model.Group) view.getChildren()
+			.get(0);
+		final org.eclipse.emf.ecp.view.model.Group subGroup = (org.eclipse.emf.ecp.view.model.Group) group
+			.getComposites().get(0);
+		group.setName(GROUP_NAME);
+
+		subGroup.setName(GROUP_NAME2);
+
+		// setup ui
+		final Shell shell = SWTViewTestHelper.createShell();
+
+		final Control control = SWTViewTestHelper.render(view, shell);
+		final Composite viewComposite = (Composite) shell.getChildren()[0];
+		final Composite renderedControl1 = (Composite) viewComposite.getChildren()[0];
+		final Control renderedControl2 = renderedControl1.getChildren()[0];
+		assertTrue("Rendered Control is not a Group", renderedControl1 instanceof org.eclipse.swt.widgets.Group);
+		assertEquals(
+			"Rendered Control and control returned by renderer are not the same",
+			control, viewComposite);
+		final Group groupControl1 = (Group) renderedControl1;
+		assertEquals("Rendered Group does not have correct name",
+			GROUP_NAME, groupControl1.getText());
+		assertEquals("org_eclipse_emf_ecp_ui_group",
+			renderedControl1.getData("org.eclipse.rap.rwt.customVariant"));
+
+		assertTrue("Rendered Control is not a Group", renderedControl2 instanceof org.eclipse.swt.widgets.Group);
+
+		final Group groupControl2 = (Group) renderedControl2;
+		assertEquals("Rendered Group does not have correct name",
+			GROUP_NAME2, groupControl2.getText());
+		assertEquals("org_eclipse_emf_ecp_ui_group",
+			renderedControl2.getData("org.eclipse.rap.rwt.customVariant"));
+	}
+
+	@Test
+	public void testEmptyGroup() throws NoRendererFoundException, NoPropertyDescriptorFoundExeption {
+
+		// setup model
+		final View view = GroupTest.createViewWithOneGroup();
+
+		// setup ui
+		final Shell shell = SWTViewTestHelper.createShell();
+		try {
+			final Composite control = (Composite) SWTViewTestHelper.render(view, shell);
+			final org.eclipse.swt.widgets.Group renderedControl = (Group) control.getChildren()[0];
+			assertEquals("", renderedControl.getText());
+		} catch (final IllegalArgumentException e) {
+			fail("Renderer throws IlleaglArgument on empty group name" + e.getStackTrace());
+		}
+	}
+}
---
+++ tests/org.eclipse.emf.ecp.view.group.ui.test/src/org/eclipse/emf/ecp/view/group/ui/test/GroupTest.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2013 EclipseSource Muenchen GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Jonas - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.ecp.view.group.ui.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.emf.ecp.internal.ui.view.renderer.Node;
+import org.eclipse.emf.ecp.ui.view.test.ViewTestHelper;
+import org.eclipse.emf.ecp.view.model.Group;
+import org.eclipse.emf.ecp.view.model.View;
+import org.eclipse.emf.ecp.view.model.ViewFactory;
+import org.junit.Test;
+
+/**
+ * @author Jonas
+ * 
+ */
+public class GroupTest {
+
+	@Test
+	public void testOneGroupinView() {
+		// setup model
+		final View view = createViewWithOneGroup();
+		final Group group = (Group) view.getChildren().get(0);
+		// Test NodeBuidlers
+		final Node<View> node = ViewTestHelper.build(view);
+		assertEquals(2, ViewTestHelper.countNodes(node));
+		assertEquals(view, node.getRenderable());
+		assertEquals("Incorrect number of nodes have been instanciated", 1, node.getChildren().size());
+		final Node<?> childNode = node.getChildren().get(0);
+		assertEquals(group, childNode.getRenderable());
+	}
+
+	/**
+	 * @return a View with one Group in it
+	 */
+	public static View createViewWithOneGroup() {
+		final View view = ViewFactory.eINSTANCE.createView();
+		final Group group = createGroup();
+		view.getChildren().add(group);
+		return view;
+	}
+
+	@Test
+	public void testTwoGroupsinView() {
+		// setup model
+		final View view = createViewWithTwoGroups();
+		final Group group = (Group) view.getChildren().get(0);
+		final Group group2 = (Group) view.getChildren().get(1);
+
+		// Test NodeBuidlers
+		final Node<View> node = ViewTestHelper.build(view);
+		assertEquals(3, ViewTestHelper.countNodes(node));
+		assertEquals(view, node.getRenderable());
+		assertEquals("Incorrect number of nodes have been instanciated", 2, node.getChildren().size());
+		final Node<?> childNode = node.getChildren().get(0);
+		assertEquals(group, childNode.getRenderable());
+		final Node<?> childNode2 = node.getChildren().get(1);
+		assertEquals(group2, childNode2.getRenderable());
+	}
+
+	/**
+	 * @return A view with two groups as children
+	 */
+	public static View createViewWithTwoGroups() {
+		final View view = ViewFactory.eINSTANCE.createView();
+		final Group group = createGroup();
+		view.getChildren().add(group);
+		final Group group2 = createGroup();
+		view.getChildren().add(group2);
+		return view;
+	}
+
+	@Test
+	public void testTwoGroupsHierachicalinView() {
+		// setup model
+		final View view = createViewWithTwoHierachicalGroups();
+		final Group group = (Group) view.getChildren().get(0);
+		final Group subGroup = (Group) group.getComposites().get(0);
+
+		// Test NodeBuidlers
+		final Node<View> node = ViewTestHelper.build(view);
+		assertEquals(3, ViewTestHelper.countNodes(node));
+		assertEquals(view, node.getRenderable());
+		assertEquals("Incorrect number of nodes have been instanciated", 1, node.getChildren().size());
+		final Node<?> childNode = node.getChildren().get(0);
+		assertEquals(group, childNode.getRenderable());
+		final Node<?> subChildNode = childNode.getChildren().get(0);
+		assertEquals(subGroup, subChildNode.getRenderable());
+	}
+
+	/**
+	 * @return A View with two groups, one is the subgroup of the first one
+	 */
+	public static View createViewWithTwoHierachicalGroups() {
+		final View view = ViewFactory.eINSTANCE.createView();
+		final Group group = createGroup();
+		view.getChildren().add(group);
+		final Group subGroup = createGroup();
+		group.getComposites().add(subGroup);
+		return view;
+	}
+
+	/**
+	 * @return
+	 */
+	private static Group createGroup() {
+		return ViewFactory.eINSTANCE.createGroup();
+	}
+
+}
