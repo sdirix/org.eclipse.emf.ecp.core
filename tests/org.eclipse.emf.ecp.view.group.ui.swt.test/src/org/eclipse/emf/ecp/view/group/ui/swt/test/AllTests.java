5b8b1153c7fc709acbe9f290b09f090991d98779
jonas <jonas.helming@googlemail.com>
2013-08-01 13:39:12 +0200

    Bug 414041 - [View Model] Extract Group in own model, added Test Cases

---
+++ tests/org.eclipse.emf.ecp.view.group.ui.swt.test/src/org/eclipse/emf/ecp/view/group/ui/swt/test/AllTests.java
@@ -0,0 +1,11 @@
+package org.eclipse.emf.ecp.view.group.ui.swt.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SWTGroupTest.class })
+public class AllTests {
+
+}
