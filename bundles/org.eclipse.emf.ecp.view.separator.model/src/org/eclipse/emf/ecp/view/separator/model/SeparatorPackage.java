ea3cce9c65ee1d5c5087861826760f52539b5079
jonas <jonas.helming@googlemail.com>
2013-08-04 14:46:26 +0200

    Bug 413984 - [View Model] Extract Separator in own model - regenerated model to fixed child descriptors

--- bundles/org.eclipse.emf.ecp.view.separator.model/src/org/eclipse/emf/ecp/view/separator/model/SeparatorPackage.java
+++ bundles/org.eclipse.emf.ecp.view.separator.model/src/org/eclipse/emf/ecp/view/separator/model/SeparatorPackage.java
@@ -4,7 +4,6 @@ package org.eclipse.emf.ecp.view.separator.model;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
-
 import org.eclipse.emf.ecp.view.model.ViewPackage;
 
 /**
@@ -12,12 +11,13 @@ import org.eclipse.emf.ecp.view.model.ViewPackage;
  * The <b>Package</b> for the model.
  * It contains accessors for the meta objects to represent
  * <ul>
- *   <li>each class,</li>
- *   <li>each feature of each class,</li>
- *   <li>each enum,</li>
- *   <li>and each data type</li>
+ * <li>each class,</li>
+ * <li>each feature of each class,</li>
+ * <li>each enum,</li>
+ * <li>and each data type</li>
  * </ul>
  * <!-- end-user-doc -->
+ * 
  * @see org.eclipse.emf.ecp.view.separator.model.SeparatorFactory
  * @model kind="package"
  * @generated
@@ -27,6 +27,7 @@ public interface SeparatorPackage extends EPackage {
 	 * The package name.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	String eNAME = "model";
@@ -35,6 +36,7 @@ public interface SeparatorPackage extends EPackage {
 	 * The package namespace URI.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	String eNS_URI = "http://org/eclipse/emf/ecp/view/separator/model";
@@ -43,6 +45,7 @@ public interface SeparatorPackage extends EPackage {
 	 * The package namespace name.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	String eNS_PREFIX = "org.eclipse.emf.ecp.view.separator.model";
@@ -51,14 +54,17 @@ public interface SeparatorPackage extends EPackage {
 	 * The singleton instance of the package.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	SeparatorPackage eINSTANCE = org.eclipse.emf.ecp.view.separator.model.impl.SeparatorPackageImpl.init();
 
 	/**
-	 * The meta object id for the '{@link org.eclipse.emf.ecp.view.separator.model.impl.SeparatorImpl <em>Separator</em>}' class.
+	 * The meta object id for the '{@link org.eclipse.emf.ecp.view.separator.model.impl.SeparatorImpl
+	 * <em>Separator</em>}' class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @see org.eclipse.emf.ecp.view.separator.model.impl.SeparatorImpl
 	 * @see org.eclipse.emf.ecp.view.separator.model.impl.SeparatorPackageImpl#getSeparator()
 	 * @generated
@@ -69,34 +75,78 @@ public interface SeparatorPackage extends EPackage {
 	 * The feature id for the '<em><b>Rule</b></em>' containment reference.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 * @ordered
 	 */
 	int SEPARATOR__RULE = ViewPackage.COMPOSITE__RULE;
 
 	/**
+	 * The feature id for the '<em><b>Visible</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 * @ordered
+	 */
+	int SEPARATOR__VISIBLE = ViewPackage.COMPOSITE__VISIBLE;
+
+	/**
+	 * The feature id for the '<em><b>Enabled</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 * @ordered
+	 */
+	int SEPARATOR__ENABLED = ViewPackage.COMPOSITE__ENABLED;
+
+	/**
+	 * The feature id for the '<em><b>Readonly</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 * @ordered
+	 */
+	int SEPARATOR__READONLY = ViewPackage.COMPOSITE__READONLY;
+
+	/**
 	 * The feature id for the '<em><b>Name</b></em>' attribute.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 * @ordered
 	 */
 	int SEPARATOR__NAME = ViewPackage.COMPOSITE__NAME;
 
 	/**
+	 * The feature id for the '<em><b>Attachments</b></em>' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 * @ordered
+	 */
+	int SEPARATOR__ATTACHMENTS = ViewPackage.COMPOSITE__ATTACHMENTS;
+
+	/**
 	 * The number of structural features of the '<em>Separator</em>' class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 * @ordered
 	 */
 	int SEPARATOR_FEATURE_COUNT = ViewPackage.COMPOSITE_FEATURE_COUNT + 0;
 
-
 	/**
-	 * Returns the meta object for class '{@link org.eclipse.emf.ecp.view.separator.model.Separator <em>Separator</em>}'.
+	 * Returns the meta object for class '{@link org.eclipse.emf.ecp.view.separator.model.Separator <em>Separator</em>}
+	 * '.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the meta object for class '<em>Separator</em>'.
 	 * @see org.eclipse.emf.ecp.view.separator.model.Separator
 	 * @generated
@@ -107,6 +157,7 @@ public interface SeparatorPackage extends EPackage {
 	 * Returns the factory that creates the instances of the model.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the factory that creates the instances of the model.
 	 * @generated
 	 */
@@ -116,19 +167,22 @@ public interface SeparatorPackage extends EPackage {
 	 * <!-- begin-user-doc -->
 	 * Defines literals for the meta objects that represent
 	 * <ul>
-	 *   <li>each class,</li>
-	 *   <li>each feature of each class,</li>
-	 *   <li>each enum,</li>
-	 *   <li>and each data type</li>
+	 * <li>each class,</li>
+	 * <li>each feature of each class,</li>
+	 * <li>each enum,</li>
+	 * <li>and each data type</li>
 	 * </ul>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	interface Literals {
 		/**
-		 * The meta object literal for the '{@link org.eclipse.emf.ecp.view.separator.model.impl.SeparatorImpl <em>Separator</em>}' class.
+		 * The meta object literal for the '{@link org.eclipse.emf.ecp.view.separator.model.impl.SeparatorImpl
+		 * <em>Separator</em>}' class.
 		 * <!-- begin-user-doc -->
 		 * <!-- end-user-doc -->
+		 * 
 		 * @see org.eclipse.emf.ecp.view.separator.model.impl.SeparatorImpl
 		 * @see org.eclipse.emf.ecp.view.separator.model.impl.SeparatorPackageImpl#getSeparator()
 		 * @generated
@@ -137,4 +191,4 @@ public interface SeparatorPackage extends EPackage {
 
 	}
 
-} //SeparatorPackage
+} // SeparatorPackage
