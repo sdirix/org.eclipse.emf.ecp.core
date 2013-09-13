d4a7aa41c0bf37c0a9d6f2faf4bca97befc21141
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-09-13 10:42:14 +0200

    Bug 416700 - Fixed compare method for VDiagnostics

--- bundles/org.eclipse.emf.ecp.view.validation/src/org/eclipse/emf/ecp/view/validation/VDiagnosticHelper.java
+++ bundles/org.eclipse.emf.ecp.view.validation/src/org/eclipse/emf/ecp/view/validation/VDiagnosticHelper.java
@@ -11,6 +11,7 @@
  ******************************************************************************/
 package org.eclipse.emf.ecp.view.validation;
 
+import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecp.view.model.VDiagnostic;
 
 /**
@@ -45,16 +46,58 @@ public final class VDiagnosticHelper {
 		if (!vDiagnostic1.getMessage().equals(vDiagnostic2.getMessage())) {
 			return false;
 		}
-		// if (vDiagnostic1.getDiagnostics().size() != vDiagnostic2.getDiagnostics().size()) {
-		// return false;
-		// }
-		// for (int i = 0; i < vDiagnostic1.getDiagnostics().size(); i++) {
-		// final Diagnostic diagnostic1 = (Diagnostic) vDiagnostic1.getDiagnostics().get(i);
-		// final Diagnostic diagnostic2 = (Diagnostic) vDiagnostic2.getDiagnostics().get(i);
-		// if (diagnostic1 != diagnostic2) {
-		// return false;
-		// }
-		// }
+		if (vDiagnostic1.getDiagnostics().size() != vDiagnostic2.getDiagnostics().size()) {
+			return false;
+		}
+
+		for (int i = 0; i < vDiagnostic1.getDiagnostics().size(); i++) {
+			final Diagnostic diagnostic1 = (Diagnostic) vDiagnostic1.getDiagnostics().get(i);
+			final Diagnostic diagnostic2 = (Diagnostic) vDiagnostic2.getDiagnostics().get(i);
+			if (!areUnderlyingDiagnosticsEqual(diagnostic1, diagnostic2)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param vDiagnostic1
+	 * @param vDiagnostic2
+	 */
+	private static boolean areUnderlyingDiagnosticsEqual(Diagnostic diagnostic1, Diagnostic diagnostic2) {
+
+		if (diagnostic1.getSeverity() != diagnostic2.getSeverity()) {
+			return false;
+		}
+
+		if (!diagnostic1.getMessage().equals(diagnostic2.getMessage())) {
+			return false;
+		}
+
+		if (diagnostic1.getData().size() != diagnostic2.getData().size()) {
+			return false;
+		}
+
+		if (diagnostic1.getChildren().size() != diagnostic2.getChildren().size()) {
+			return false;
+		}
+
+		for (int j = 0; j < diagnostic1.getData().size(); j++) {
+			final Object data1 = diagnostic1.getData().get(j);
+			final Object data2 = diagnostic2.getData().get(j);
+
+			if (!data1.equals(data2)) {
+				return false;
+			}
+		}
+
+		for (int i = 0; i < diagnostic1.getChildren().size(); i++) {
+			if (!areUnderlyingDiagnosticsEqual(diagnostic1.getChildren().get(i), diagnostic2.getChildren().get(i))) {
+				return false;
+			}
+		}
+
 		return true;
 	}
 }
