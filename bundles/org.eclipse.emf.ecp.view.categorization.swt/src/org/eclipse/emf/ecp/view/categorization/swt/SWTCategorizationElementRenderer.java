7b1749d6f346b2a72e31d0b2eff8284e27774354
eneufeld <neufeld.eugen@googlemail.com>
2013-11-14 00:19:43 +0100

    415460: [View Model] Extract hierarchy elements from core model https://bugs.eclipse.org/bugs/show_bug.cgi?id=415460 - updated tests

--- bundles/org.eclipse.emf.ecp.view.categorization.swt/src/org/eclipse/emf/ecp/view/categorization/swt/SWTCategorizationElementRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.categorization.swt/src/org/eclipse/emf/ecp/view/categorization/swt/SWTCategorizationElementRenderer.java
@@ -15,7 +15,6 @@ package org.eclipse.emf.ecp.view.categorization.swt;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
---
+++ tests/org.eclipse.emf.ecp.view.categorization.swt.test/src/org/eclipse/emf/ecp/view/categorization/swt/test/AllTests.java
@@ -0,0 +1,11 @@
+package org.eclipse.emf.ecp.view.categorization.swt.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CategorizationTests.class})
+public class AllTests {
+
+}
