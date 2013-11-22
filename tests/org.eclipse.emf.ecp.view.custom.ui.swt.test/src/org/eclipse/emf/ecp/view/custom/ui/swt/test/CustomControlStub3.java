4b185626e1b575ff5372acc5c3aa49518d284c87
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-11-22 10:16:42 +0100

    Bug 412111 - Add Unit tests Added further unit tests for CustomControls https://bugs.eclipse.org/bugs/show_bug.cgi?id=412111

---
+++ tests/org.eclipse.emf.ecp.view.custom.ui.swt.test/src/org/eclipse/emf/ecp/view/custom/ui/swt/test/CustomControlStub3.java
@@ -0,0 +1,148 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2013 EclipseSource Muenchen GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Johannes Faltermeier - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.ecp.view.custom.ui.swt.test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.databinding.property.value.IValueProperty;
+import org.eclipse.emf.databinding.EMFProperties;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecp.edit.internal.swt.util.SWTRenderingHelper;
+import org.eclipse.emf.ecp.internal.ui.view.renderer.RenderingResultRow;
+import org.eclipse.emf.ecp.ui.view.custom.swt.ECPAbstractCustomControlSWT;
+import org.eclipse.emf.ecp.view.custom.model.ECPCustomControlChangeListener;
+import org.eclipse.emf.ecp.view.model.VDomainModelReference;
+import org.eclipse.emf.ecp.view.model.VFeaturePathDomainModelReference;
+import org.eclipse.emf.ecp.view.model.VViewFactory;
+import org.eclipse.emf.emfstore.bowling.BowlingPackage;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * @author jfaltermeier
+ * 
+ */
+public class CustomControlStub3 extends ECPAbstractCustomControlSWT {
+	private static VFeaturePathDomainModelReference playersReference;
+	private static VFeaturePathDomainModelReference nameReference;
+	private static VFeaturePathDomainModelReference dateReference;
+	private static Set<VDomainModelReference> features = new LinkedHashSet<VDomainModelReference>();
+	static {
+		playersReference = VViewFactory.eINSTANCE
+			.createFeaturePathDomainModelReference();
+		playersReference.setDomainModelEFeature(BowlingPackage.eINSTANCE.getLeague_Players());
+
+		nameReference = VViewFactory.eINSTANCE
+			.createFeaturePathDomainModelReference();
+		nameReference.setDomainModelEFeature(BowlingPackage.eINSTANCE.getPlayer_Name());
+		nameReference.getDomainModelEReferencePath().add(BowlingPackage.eINSTANCE.getLeague_Players());
+
+		dateReference = VViewFactory.eINSTANCE.createFeaturePathDomainModelReference();
+		dateReference.setDomainModelEFeature(BowlingPackage.eINSTANCE.getPlayer_DateOfBirth());
+		dateReference.getDomainModelEReferencePath().add(BowlingPackage.eINSTANCE.getLeague_Players());
+
+		features.add(playersReference);
+		features.add(nameReference);
+		features.add(dateReference);
+	}
+
+	public static final String CHANGE_NOTICED = "CHANGE!";
+
+	private Label label;
+
+	public CustomControlStub3() {
+		super(features);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.ecp.ui.view.custom.swt.ECPAbstractCustomControlSWT#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected List<RenderingResultRow<Control>> createControl(Composite composite) {
+		final List<RenderingResultRow<Control>> result = new ArrayList<RenderingResultRow<Control>>();
+		label = new Label(composite, SWT.NONE);
+		label.setText("Players: ");
+
+		final Composite tableParent = new Composite(composite, SWT.NONE);
+
+		final Composite tableComposite = new Composite(tableParent, SWT.NONE);
+		final TableViewer tableViewer = new TableViewer(tableComposite, SWT.BORDER | SWT.FULL_SELECTION
+			| SWT.V_SCROLL);
+		tableViewer.getControl().setLayoutData(new GridData());
+		tableComposite.setLayout(new TableColumnLayout());
+
+		final IValueProperty[] valueProperties = EMFProperties.values(BowlingPackage.Literals.PLAYER__NAME,
+			BowlingPackage.Literals.PLAYER__DATE_OF_BIRTH);
+
+		createViewerBinding(playersReference, tableViewer, valueProperties);
+
+		registerChangeListener(playersReference, new LeagueChangeListener());
+
+		result.add(SWTRenderingHelper.INSTANCE.getResultRowFactory()
+			.createRenderingResultRow(label, tableParent));
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.ecp.ui.view.custom.swt.ECPAbstractCustomControlSWT#handleContentValidation(int,
+	 *      org.eclipse.emf.ecore.EStructuralFeature)
+	 */
+	@Override
+	protected void handleContentValidation(int severity, EStructuralFeature feature) {
+		// nothing to do
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.ecp.ui.view.custom.swt.ECPAbstractCustomControlSWT#resetContentValidation()
+	 */
+	@Override
+	protected void resetContentValidation() {
+		// nothing to do
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.ecp.ui.view.custom.ECPAbstractCustomControl#disposeCustomControl()
+	 */
+	@Override
+	protected void disposeCustomControl() {
+
+	}
+
+	public class LeagueChangeListener implements ECPCustomControlChangeListener {
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @see org.eclipse.emf.ecp.view.custom.model.ECPCustomControlChangeListener#notifyChanged()
+		 */
+		public void notifyChanged() {
+			label.setText(CHANGE_NOTICED);
+		}
+
+	}
+
+}
