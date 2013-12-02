5e9b064a67f5661ea0c65cf511ea723fe43ccd52
eneufeld <neufeld.eugen@googlemail.com>
2013-12-02 12:10:58 +0100

    415460: [View Model] Extract hierarchy elements from core model https://bugs.eclipse.org/bugs/show_bug.cgi?id=415460 added tests

--- tests/org.eclipse.emf.ecp.view.categorization.swt.test/src/org/eclipse/emf/ecp/view/categorization/swt/test/AllTests.java
+++ tests/org.eclipse.emf.ecp.view.categorization.swt.test/src/org/eclipse/emf/ecp/view/categorization/swt/test/AllTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CategorizationTests.class, CategorizationTreeRefreshTest.class })
+@SuiteClasses({ CategorizationTests.class, CategorizationTreeRefreshTest.class,
+	CategorizationTreeContentRefreshTest.class })
 public class AllTests {
 
 }
