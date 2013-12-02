fedcfa4ca481c52db24048ed5753de6d95ee89d0
emueller <edgar.m.mueller@gmail.com>
2013-12-02 18:03:43 +0100

    Bug 412104: Fix Validation error display in TableControl https://bugs.eclipse.org/bugs/show_bug.cgi?id=412104, removed Table.ALWAYS_HIDE_SELECTIO occurence

--- bundles/org.eclipse.emf.ecp.edit.swt/src/org/eclipse/emf/ecp/edit/internal/swt/controls/ECPFocusCellDrawHighlighter.java
+++ bundles/org.eclipse.emf.ecp.edit.swt/src/org/eclipse/emf/ecp/edit/internal/swt/controls/ECPFocusCellDrawHighlighter.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2013 EclipseSource Muenchen GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Edgar Mueller - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.emf.ecp.edit.internal.swt.controls;
 
 import org.eclipse.jface.viewers.CellLabelProvider;
@@ -46,7 +57,7 @@ public class ECPFocusCellDrawHighlighter extends FocusCellHighlighter {
 		this.viewer = viewer;
 		// hookListener(viewer);
 		// RAP [if]
-		viewer.getControl().setData(Table.ALWAYS_HIDE_SELECTION, Boolean.TRUE);
+		viewer.getControl().setData(Table.class.getName() + "#alwaysHideSelection", Boolean.TRUE);
 	}
 
 	private void markFocusedCell(Event event, ViewerCell cell) {
