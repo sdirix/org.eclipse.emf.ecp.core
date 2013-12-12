884e6370946b15fa7ce09c1bd6ca437841ae0200
eneufeld <neufeld.eugen@googlemail.com>
2013-12-12 16:17:28 +0100

    423922: [View Editor] Resolvement ignores containments of parent classes https://bugs.eclipse.org/bugs/show_bug.cgi?id=423922

--- bundles/org.eclipse.emf.ecp.view.model/src/org/eclipse/emf/ecp/view/spi/model/impl/VFeaturePathDomainModelReferenceImpl.java
+++ bundles/org.eclipse.emf.ecp.view.model/src/org/eclipse/emf/ecp/view/spi/model/impl/VFeaturePathDomainModelReferenceImpl.java
@@ -108,14 +108,15 @@ public class VFeaturePathDomainModelReferenceImpl extends EObjectImpl implements
 	{
 		if (domainModelEFeature != null && domainModelEFeature.eIsProxy())
 		{
-			InternalEObject oldDomainModelEFeature = (InternalEObject) domainModelEFeature;
+			final InternalEObject oldDomainModelEFeature = (InternalEObject) domainModelEFeature;
 			domainModelEFeature = (EStructuralFeature) eResolveProxy(oldDomainModelEFeature);
 			if (domainModelEFeature != oldDomainModelEFeature)
 			{
-				if (eNotificationRequired())
+				if (eNotificationRequired()) {
 					eNotify(new ENotificationImpl(this, Notification.RESOLVE,
 						VViewPackage.FEATURE_PATH_DOMAIN_MODEL_REFERENCE__DOMAIN_MODEL_EFEATURE,
 						oldDomainModelEFeature, domainModelEFeature));
+				}
 			}
 		}
 		return domainModelEFeature;
@@ -140,12 +141,13 @@ public class VFeaturePathDomainModelReferenceImpl extends EObjectImpl implements
 	 */
 	public void setDomainModelEFeature(EStructuralFeature newDomainModelEFeature)
 	{
-		EStructuralFeature oldDomainModelEFeature = domainModelEFeature;
+		final EStructuralFeature oldDomainModelEFeature = domainModelEFeature;
 		domainModelEFeature = newDomainModelEFeature;
-		if (eNotificationRequired())
+		if (eNotificationRequired()) {
 			eNotify(new ENotificationImpl(this, Notification.SET,
 				VViewPackage.FEATURE_PATH_DOMAIN_MODEL_REFERENCE__DOMAIN_MODEL_EFEATURE, oldDomainModelEFeature,
 				domainModelEFeature));
+		}
 	}
 
 	/**
@@ -176,8 +178,9 @@ public class VFeaturePathDomainModelReferenceImpl extends EObjectImpl implements
 		switch (featureID)
 		{
 		case VViewPackage.FEATURE_PATH_DOMAIN_MODEL_REFERENCE__DOMAIN_MODEL_EFEATURE:
-			if (resolve)
+			if (resolve) {
 				return getDomainModelEFeature();
+			}
 			return basicGetDomainModelEFeature();
 		case VViewPackage.FEATURE_PATH_DOMAIN_MODEL_REFERENCE__DOMAIN_MODEL_EREFERENCE_PATH:
 			return getDomainModelEReferencePath();
@@ -295,7 +298,7 @@ public class VFeaturePathDomainModelReferenceImpl extends EObjectImpl implements
 			if (eReference.isMany()) {
 				break;
 			}
-			if (!eReference.eContainer().equals(currentResolvedEObject.eClass())) {
+			if (!eReference.getEContainingClass().isInstance(currentResolvedEObject)) {
 				return false;
 			}
 			EObject child = (EObject) currentResolvedEObject.eGet(eReference);
