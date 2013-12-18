ca275cbb2cafd282894f2135ba028d1fc501a5b7
emueller <edgar.m.mueller@gmail.com>
2013-12-18 13:48:18 +0100

    Bug 416879 - [e4] Context Menu does not display possible child elements , added missing files & removed warnings

---
+++ bundles/org.eclipse.emf.ecp.ui.e4/src/org/eclipse/emf/ecp/ui/e4/view/DynamicCreateChildrenElementsMenuContribution.java
@@ -0,0 +1,137 @@
+package org.eclipse.emf.ecp.ui.e4.view;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.di.AboutToShow;
+import org.eclipse.e4.ui.model.application.ui.menu.MDirectMenuItem;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenuFactory;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecp.common.ChildrenDescriptorCollector;
+import org.eclipse.emf.ecp.core.ECPProject;
+import org.eclipse.emf.ecp.core.util.ECPUtil;
+import org.eclipse.emf.ecp.internal.ui.util.ECPHandlerHelper;
+import org.eclipse.emf.edit.command.CommandActionDelegate;
+import org.eclipse.emf.edit.command.CommandParameter;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.ui.action.CreateChildAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+
+/**
+ * @author krusche
+ * 
+ */
+@SuppressWarnings("restriction")
+public class DynamicCreateChildrenElementsMenuContribution {
+	@Inject
+	private ESelectionService selectionService;
+
+	public List<?> getSelectedObjects() {
+		final Object sel = selectionService.getSelection();
+		if (sel != null) {
+			if (sel instanceof Collection) {
+				Collection<?> col = (Collection<?>) sel;
+				return new ArrayList<Object>(col);
+			} else if (sel instanceof Object[]) {
+				return Arrays.asList((Object[]) sel);
+			} else if (sel instanceof IStructuredSelection) {
+				IStructuredSelection ssel = (IStructuredSelection) sel;
+				return ssel.toList();
+			} else {
+				return Arrays.asList(sel);
+			}
+		}
+
+		return null;
+	}
+
+	@AboutToShow
+	public void aboutToShow(List<MMenuElement> items) {
+		final List<?> selectedObjects = getSelectedObjects();
+		if (selectedObjects == null || selectedObjects.isEmpty()) {
+			return;
+		}
+		final Object[] elements = selectedObjects.toArray();
+
+		if (elements.length == 1 && elements[0] instanceof EObject) {
+			final EObject eObject = (EObject) elements[0];
+			final ECPProject project = ECPUtil.getECPProjectManager()
+					.getProject(eObject);
+			final EditingDomain domain = project.getEditingDomain();
+			final Collection<?> childDescriptors = new ChildrenDescriptorCollector()
+					.getDescriptors(eObject);
+
+			for (final Object childDescriptor : childDescriptors) {
+				final CommandParameter cp = (CommandParameter) childDescriptor;
+				if (cp.getEReference() == null) {
+					continue;
+				}
+				if (!cp.getEReference().isMany()
+						&& eObject.eIsSet(cp.getEStructuralFeature())) {
+					continue;
+				} else if (cp.getEReference().isMany()
+						&& cp.getEReference().getUpperBound() != -1
+						&& cp.getEReference().getUpperBound() <= ((List<?>) eObject
+								.eGet(cp.getEReference())).size()) {
+					continue;
+				}
+
+				final CustomCreateChildAction createChildAction = new CustomCreateChildAction(
+						domain, new StructuredSelection(eObject),
+						childDescriptor, cp, project);
+
+				final MDirectMenuItem dynamicItem = MMenuFactory.INSTANCE
+						.createDirectMenuItem();
+
+				dynamicItem.setLabel(createChildAction.getText());
+				dynamicItem.setTooltip(createChildAction.getToolTipText());
+				dynamicItem.setObject(new Object() {
+					@Execute
+					public void execute(MDirectMenuItem me) {
+						createChildAction.run();
+					}
+				});
+
+				dynamicItem.setIconURI(createChildAction.getImageURIString());
+				items.add(dynamicItem);
+			}
+		}
+	}
+
+	class CustomCreateChildAction extends CreateChildAction {
+
+		public CustomCreateChildAction(EditingDomain editingDomain,
+				ISelection selection, Object descriptor, CommandParameter cp,
+				ECPProject project) {
+			super(editingDomain, selection, descriptor);
+			this.cp = cp;
+			this.project = project;
+		}
+
+		private final CommandParameter cp;
+
+		private final ECPProject project;
+
+		@Override
+		public void run() {
+			super.run();
+			ECPHandlerHelper.openModelElement(cp.getEValue(), project);
+		}
+
+		public String getImageURIString() {
+			final CommandActionDelegate commandActionDelegate = (CommandActionDelegate) command;
+
+			return commandActionDelegate.getImage().toString();
+		}
+
+	}
+}
