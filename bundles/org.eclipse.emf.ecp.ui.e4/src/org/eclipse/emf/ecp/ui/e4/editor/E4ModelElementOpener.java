5bd3aec5f47ed2273b55ba2a4c4f3cad42b1191a
eneufeld <neufeld.eugen@googlemail.com>
2014-01-10 11:50:28 +0100

    424393: Improve e4 Integration https://bugs.eclipse.org/bugs/show_bug.cgi?id=424393 reverted getpartservice method

--- bundles/org.eclipse.emf.ecp.ui.e4/src/org/eclipse/emf/ecp/ui/e4/editor/E4ModelElementOpener.java
+++ bundles/org.eclipse.emf.ecp.ui.e4/src/org/eclipse/emf/ecp/ui/e4/editor/E4ModelElementOpener.java
@@ -12,7 +12,8 @@
  *******************************************************************************/
 package org.eclipse.emf.ecp.ui.e4.editor;
 
-import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.contexts.IContextFunction;
+import org.eclipse.e4.ui.internal.workbench.E4Workbench;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
@@ -20,6 +21,7 @@ import org.eclipse.emf.ecp.core.ECPProject;
 import org.eclipse.emf.ecp.ui.util.ECPModelElementOpener;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 
 /**
@@ -68,11 +70,20 @@ public class E4ModelElementOpener implements ECPModelElementOpener {
 	 * 
 	 * @return the part service of the current window.
 	 */
-	public static EPartService getEPartService() {
-		final BundleContext bundleContext = FrameworkUtil.getBundle(E4ModelElementOpener.class).getBundleContext();
-		final ServiceReference<?> serviceReference = bundleContext.getServiceReference(IEclipseContext.class);
-		final IEclipseContext context = (IEclipseContext) bundleContext.getService(serviceReference);
-		final EPartService ePartService = context.get(EPartService.class);
-		return ePartService;
+	private EPartService getEPartService() {
+		final BundleContext bundleContext = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+		ServiceReference<?> service;
+		try {
+			service = bundleContext.getServiceReferences(
+				IContextFunction.class.getName(), "(service.context.key="
+					+ EPartService.class.getName() + ")")[0];
+			// TODO a "bit" ugly
+			return (EPartService) ((IContextFunction) bundleContext.getService(service))
+				.compute(E4Workbench.getServiceContext(), null);
+		} catch (final InvalidSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
