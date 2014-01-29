9a82fa5a2e97e7af5a9ce27111b3fa8f6e6d7929
eneufeld <neufeld.eugen@googlemail.com>
2014-01-29 13:24:38 +0100

    426794: Separator without name break editor https://bugs.eclipse.org/bugs/show_bug.cgi?id=426794

--- bundles/org.eclipse.emf.ecp.view.separator.ui.swt/src/org/eclipse/emf/ecp/view/spi/separator/swt/SWTSeparatorRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.separator.ui.swt/src/org/eclipse/emf/ecp/view/spi/separator/swt/SWTSeparatorRenderer.java
@@ -49,7 +49,9 @@ public class SWTSeparatorRenderer extends AbstractSWTRenderer<VSeparator> {
 		throws NoRendererFoundException,
 		NoPropertyDescriptorFoundExeption {
 		final Label label = new Label(parent, SWT.NONE);
-		label.setText(separator.getName());
+		if (separator.getName() != null) {
+			label.setText(separator.getName());
+		}
 		label.setData(CUSTOM_VARIANT, "org_eclipse_emf_ecp_ui_seperator"); //$NON-NLS-1$
 
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER)
