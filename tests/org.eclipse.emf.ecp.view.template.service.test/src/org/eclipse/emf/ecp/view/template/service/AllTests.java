69977d1bc5ebfead77bca4049f6236b31199998b
eneufeld <neufeld.eugen@googlemail.com>
2014-04-01 17:18:41 +0200

    431720: [View Model] TextFields should alignable https://bugs.eclipse.org/bugs/show_bug.cgi?id=431720

---
+++ tests/org.eclipse.emf.ecp.view.template.service.test/src/org/eclipse/emf/ecp/view/template/service/AllTests.java
@@ -0,0 +1,11 @@
+package org.eclipse.emf.ecp.view.template.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ViewTemplateServiceTest.class,DomainModelReferenceSelectorTest.class,AlignmentStyleTest.class})
+public class AllTests {
+
+}
