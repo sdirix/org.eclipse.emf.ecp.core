0723f1d33393032adcdd362c56ffb4c45179290c
AlexandraBuzila <alexandra.buzila@gmail.com>
2014-05-02 11:55:59 +0200

    426674: [View Model IDE] Make View Models editable within the IDE https://bugs.eclipse.org/bugs/show_bug.cgi?id=426674 Programatically add buttons to PreviewView toolbar.

--- bundles/org.eclipse.emf.ecp.view.model.preview.e3/src/org/eclipse/emf/ecp/view/model/internal/preview/e3/views/PreviewView.java
+++ bundles/org.eclipse.emf.ecp.view.model.preview.e3/src/org/eclipse/emf/ecp/view/model/internal/preview/e3/views/PreviewView.java
@@ -20,6 +20,9 @@ import org.eclipse.emf.ecp.view.model.internal.preview.Activator;
 import org.eclipse.emf.ecp.view.model.preview.common.Preview;
 import org.eclipse.emf.ecp.view.spi.model.VView;
 import org.eclipse.emf.ecp.view.spi.model.VViewPackage;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ISelection;
@@ -51,6 +54,7 @@ public class PreviewView extends ViewPart implements ISelectionListener {
 	private VView view;
 	private boolean updateAutomatic;
 	private Composite parent;
+	private Action automaticToggleButton, manualRefreshButton;
 
 	/** The constructor. */
 	public PreviewView() {
@@ -89,6 +93,8 @@ public class PreviewView extends ViewPart implements ISelectionListener {
 			.getBundle()
 			.getResource("icons/preview.png")); //$NON-NLS-1$
 		form.setImage(new Image(parent.getDisplay(), imageDescriptor.getImageData()));
+
+		addButtonsToFormToolbar(form.getToolBarManager());
 
 		preView = new Preview(form.getBody());
 
@@ -169,6 +175,53 @@ public class PreviewView extends ViewPart implements ISelectionListener {
 		};
 		getSite().getPage().addPartListener(partListener);
 
+	}
+
+	/**
+	 * @param toolBarManager
+	 */
+	private void addButtonsToFormToolbar(IToolBarManager toolBarManager) {
+		// toggle automatic refresh state button
+
+		// automatic refresh
+		automaticToggleButton = new Action("", IAction.AS_CHECK_BOX) { //$NON-NLS-1$
+			@Override
+			public void run() {
+				super.run();
+				setUpdateAutomatic(isChecked());
+				manualRefreshButton.setEnabled(!isChecked());
+			}
+		};
+
+		final String autoRefreshImagePath = "icons/arrow_rotate_anticlockwise.png";//$NON-NLS-1$
+		automaticToggleButton.setImageDescriptor(ImageDescriptor.createFromURL(Activator.getDefault()
+			.getBundle()
+			.getResource(autoRefreshImagePath)));
+
+		automaticToggleButton.setText("Automatically refresh Preview View"); //$NON-NLS-1$
+		automaticToggleButton.setEnabled(true);
+		automaticToggleButton.setChecked(false);
+
+		// manual refresh
+		manualRefreshButton = new Action() {
+			@Override
+			public void run() {
+				super.run();
+				render();
+			}
+		};
+		final String manualRefreshImagePath = "icons/arrow_refresh.png";//$NON-NLS-1$
+		manualRefreshButton.setImageDescriptor(ImageDescriptor.createFromURL(Activator.getDefault()
+			.getBundle()
+			.getResource(manualRefreshImagePath)));
+
+		manualRefreshButton.setText("Refresh Preview View"); //$NON-NLS-1$
+		manualRefreshButton.setEnabled(true);
+
+		toolBarManager.add(manualRefreshButton);
+		toolBarManager.add(automaticToggleButton);
+
+		toolBarManager.update(true);
 	}
 
 	/**
