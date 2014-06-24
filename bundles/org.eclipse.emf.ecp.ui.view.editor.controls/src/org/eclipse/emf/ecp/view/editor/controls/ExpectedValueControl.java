21301821da971a479ad7608594cce217eee37c09
abuzila <alexandra.buzila@gmail.com>
2014-06-24 17:20:29 +0200

    Bug 437585 - LeafCondition value selection for enums doesn't work https://bugs.eclipse.org/bugs/show_bug.cgi?id=437585
    Change-Id: Icc3d964715b37052eaebb621fd45cd0fd5be506e
    Signed-off-by: abuzila <alexandra.buzila@gmail.com>

--- bundles/org.eclipse.emf.ecp.ui.view.editor.controls/src/org/eclipse/emf/ecp/view/editor/controls/ExpectedValueControl.java
+++ bundles/org.eclipse.emf.ecp.ui.view.editor.controls/src/org/eclipse/emf/ecp/view/editor/controls/ExpectedValueControl.java
@@ -16,9 +16,13 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.databinding.Binding;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EEnumLiteral;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.impl.EEnumImpl;
 import org.eclipse.emf.ecp.edit.internal.swt.controls.SingleControl;
 import org.eclipse.emf.ecp.view.spi.model.VFeaturePathDomainModelReference;
 import org.eclipse.emf.ecp.view.spi.rule.model.LeafCondition;
@@ -90,8 +94,8 @@ public class ExpectedValueControl extends SingleControl {
 				if (condition.getDomainModelReference() == null) {
 					MessageDialog.openError(text.getShell(), "No Feature Path Domain Model Reference found", //$NON-NLS-1$
 						"A Feature Path Domain Model Reference needs to be added to the condition first. " //$NON-NLS-1$
-						// + "Without it we can't provide you with support!"
-					);
+					// + "Without it we can't provide you with support!"
+						);
 					return;
 				}
 				final EStructuralFeature structuralFeature = ((VFeaturePathDomainModelReference) condition
@@ -99,72 +103,84 @@ public class ExpectedValueControl extends SingleControl {
 				if (structuralFeature == null) {
 					MessageDialog.openError(text.getShell(), "No Attribute selected", //$NON-NLS-1$
 						"Please select an attribute first. " //$NON-NLS-1$
-						// + "Without the attribute we can't provide you with support!"
-					);
+					// + "Without the attribute we can't provide you with support!"
+						);
 					return;
 				}
 				if (EReference.class.isInstance(structuralFeature)) {
 					// TODO show all references
 					return;
 				}
+
+				Object object = null;
 				final EAttribute attribute = (EAttribute) structuralFeature;
 				Class<?> attribuetClazz = attribute.getEAttributeType().getInstanceClass();
-				if (attribuetClazz.isPrimitive()) {
-					if (int.class.isAssignableFrom(attribuetClazz)) {
-						attribuetClazz = Integer.class;
-					} else if (long.class.isAssignableFrom(attribuetClazz)) {
-						attribuetClazz = Long.class;
-					} else if (float.class.isAssignableFrom(attribuetClazz)) {
-						attribuetClazz = Float.class;
-					} else if (double.class.isAssignableFrom(attribuetClazz)) {
-						attribuetClazz = Double.class;
-					} else if (boolean.class.isAssignableFrom(attribuetClazz)) {
-						attribuetClazz = Boolean.class;
-					} else if (char.class.isAssignableFrom(attribuetClazz)) {
-						attribuetClazz = Character.class;
-					}
-				}
-				Object object = null;
-				if (Enum.class.isAssignableFrom(attribuetClazz)) {
-					final Object[] enumValues = attribuetClazz.getEnumConstants();
-					final ListDialog ld = new ListDialog(text.getShell());
-					ld.setLabelProvider(new LabelProvider());
-					ld.setContentProvider(ArrayContentProvider.getInstance());
-					ld.setInput(enumValues);
-					ld.setInitialSelections(new Object[] { enumValues[0] });
-					ld.setMessage("Please select the enum value to set."); //$NON-NLS-1$
-					ld.setTitle("Select a value"); //$NON-NLS-1$
-					final int enumSelectionResult = ld.open();
-					if (Window.OK == enumSelectionResult) {
-						object = ld.getResult()[0];
-					}
-				} else if (String.class.isAssignableFrom(attribuetClazz)
-					|| Number.class.isAssignableFrom(attribuetClazz) || Boolean.class.isAssignableFrom(attribuetClazz)) {
-					try {
-						final Constructor<?> constructor = attribuetClazz.getConstructor(String.class);
-						final InputDialog id = new InputDialog(
-							text.getShell(),
-							"Insert the value", //$NON-NLS-1$
-							"The value must be parseable by the " //$NON-NLS-1$
-								+ attribuetClazz.getSimpleName()
-								+ " class. For a double value please use the #.# format. For boolean values 'true' or 'false'.", //$NON-NLS-1$
-							null, null);
-						final int inputResult = id.open();
-						if (Window.OK == inputResult) {
-							object = constructor.newInstance(id.getValue());
+
+				if (attribuetClazz == null) {
+					if (EcorePackage.eINSTANCE.getEEnum().isInstance(attribute.getEType())) {
+						attribuetClazz = Enum.class;
+						final EList<EEnumLiteral> eLiterals = EEnumImpl.class.cast(attribute.getEType()).getELiterals();
+						final Object[] enumValues = eLiterals.toArray();
+						final ListDialog ld = new ListDialog(text.getShell());
+						ld.setLabelProvider(new LabelProvider());
+						ld.setContentProvider(ArrayContentProvider.getInstance());
+						ld.setInput(enumValues);
+						ld.setInitialSelections(new Object[] { enumValues[0] });
+						ld.setMessage("Please select the enum value to set."); //$NON-NLS-1$
+						ld.setTitle("Select a value"); //$NON-NLS-1$
+						final int enumSelectionResult = ld.open();
+						if (Window.OK == enumSelectionResult) {
+							object = ld.getResult()[0];
 						}
-					} catch (final IllegalArgumentException ex) {
-
-					} catch (final SecurityException ex) {
-
-					} catch (final NoSuchMethodException ex) {
-					} catch (final InstantiationException ex) {
-					} catch (final IllegalAccessException ex) {
-					} catch (final InvocationTargetException ex) {
+					} else {
+						return;
 					}
 				} else {
-					MessageDialog.openError(text.getShell(), "Not primitive Attribute selected", //$NON-NLS-1$
-						"The selected attribute has a not primitive type. We can't provide you support for it!"); //$NON-NLS-1$
+					if (attribuetClazz.isPrimitive()) {
+						if (int.class.isAssignableFrom(attribuetClazz)) {
+							attribuetClazz = Integer.class;
+						} else if (long.class.isAssignableFrom(attribuetClazz)) {
+							attribuetClazz = Long.class;
+						} else if (float.class.isAssignableFrom(attribuetClazz)) {
+							attribuetClazz = Float.class;
+						} else if (double.class.isAssignableFrom(attribuetClazz)) {
+							attribuetClazz = Double.class;
+						} else if (boolean.class.isAssignableFrom(attribuetClazz)) {
+							attribuetClazz = Boolean.class;
+						} else if (char.class.isAssignableFrom(attribuetClazz)) {
+							attribuetClazz = Character.class;
+						}
+					}
+
+					if (String.class.isAssignableFrom(attribuetClazz)
+						|| Number.class.isAssignableFrom(attribuetClazz)
+						|| Boolean.class.isAssignableFrom(attribuetClazz)) {
+						try {
+							final Constructor<?> constructor = attribuetClazz.getConstructor(String.class);
+							final InputDialog id = new InputDialog(
+								text.getShell(),
+								"Insert the value", //$NON-NLS-1$
+								"The value must be parseable by the " //$NON-NLS-1$
+									+ attribuetClazz.getSimpleName()
+									+ " class. For a double value please use the #.# format. For boolean values 'true' or 'false'.", //$NON-NLS-1$
+								null, null);
+							final int inputResult = id.open();
+							if (Window.OK == inputResult) {
+								object = constructor.newInstance(id.getValue());
+							}
+						} catch (final IllegalArgumentException ex) {
+
+						} catch (final SecurityException ex) {
+
+						} catch (final NoSuchMethodException ex) {
+						} catch (final InstantiationException ex) {
+						} catch (final IllegalAccessException ex) {
+						} catch (final InvocationTargetException ex) {
+						}
+					} else {
+						MessageDialog.openError(text.getShell(), "Not primitive Attribute selected", //$NON-NLS-1$
+							"The selected attribute has a not primitive type. We can't provide you support for it!"); //$NON-NLS-1$
+					}
 				}
 
 				if (object != null) {
@@ -174,6 +190,7 @@ public class ExpectedValueControl extends SingleControl {
 							RulePackage.eINSTANCE.getLeafCondition_ExpectedValue(), object));
 					text.setText(object.toString());
 				}
+
 			}
 
 		});
