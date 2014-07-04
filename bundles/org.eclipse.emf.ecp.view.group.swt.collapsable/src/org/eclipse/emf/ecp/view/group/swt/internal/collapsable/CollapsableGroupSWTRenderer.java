2dd804a66e5c54da176b37bbcb2740b5e9a6d473
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2014-07-04 16:19:33 +0200

    Bug 438253 - Add collapsible state to group view model * Relayout on expand/collapse * Because of problems with GridLayouts/Data and the timing when the ExpandListener is called the adjustments are kinda bogus. See comments. https://bugs.eclipse.org/bugs/show_bug.cgi?id=438253

--- bundles/org.eclipse.emf.ecp.view.group.swt.collapsable/src/org/eclipse/emf/ecp/view/group/swt/internal/collapsable/CollapsableGroupSWTRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.group.swt.collapsable/src/org/eclipse/emf/ecp/view/group/swt/internal/collapsable/CollapsableGroupSWTRenderer.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2011-2014 EclipseSource Muenchen GmbH and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * EclipseSource Munich - initial API and implementation
  */
@@ -20,6 +20,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ExpandEvent;
 import org.eclipse.swt.events.ExpandListener;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.ExpandBar;
@@ -27,28 +28,34 @@ import org.eclipse.swt.widgets.ExpandItem;
 
 /**
  * A Group renderer, which is collapsable.
- * 
+ *
  * @author Eugen Neufeld
- * 
+ * @author jfaltermeier
+ *
  */
 @SuppressWarnings("restriction")
 public class CollapsableGroupSWTRenderer extends ContainerSWTRenderer<VGroup> {
 
+	private static final int MARGIN = 5;
+
 	/**
 	 * {@inheritDoc}
-	 * 
+	 *
 	 * @see org.eclipse.emf.ecp.view.spi.core.swt.ContainerSWTRenderer#renderControl(org.eclipse.emf.ecp.view.spi.swt.layout.SWTGridCell,
 	 *      org.eclipse.swt.widgets.Composite)
 	 */
 	@Override
-	protected Control renderControl(SWTGridCell gridCell, Composite parent) throws NoRendererFoundException,
-		NoPropertyDescriptorFoundExeption {
+	protected Control renderControl(SWTGridCell gridCell, final Composite parent) throws NoRendererFoundException,
+	NoPropertyDescriptorFoundExeption {
 		final ExpandBar bar = new ExpandBar(parent, SWT.V_SCROLL);
 		bar.setBackground(parent.getBackground());
 
 		// First item
 		final Composite composite = new Composite(bar, SWT.NONE);
-		composite.setLayout(new FillLayout());
+		final FillLayout fillLayout = new FillLayout();
+		fillLayout.marginHeight = MARGIN;
+		fillLayout.marginWidth = MARGIN;
+		composite.setLayout(fillLayout);
 		final ExpandItem item0 = new ExpandItem(bar, SWT.NONE, 0);
 		String text = getVElement().getName();
 		if (text == null) {
@@ -56,23 +63,46 @@ public class CollapsableGroupSWTRenderer extends ContainerSWTRenderer<VGroup> {
 		}
 		item0.setText(text);
 		super.renderControl(gridCell, composite);
-		item0.setHeight(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		final int height = computeHeight(composite);
+		item0.setHeight(height);
 		item0.setControl(composite);
 		bar.addExpandListener(new ExpandListener() {
 
 			@Override
 			public void itemCollapsed(ExpandEvent e) {
+				item0.setHeight(item0.getHeaderHeight());
+				final Object layoutData = bar.getLayoutData();
+				updateLayoutData(layoutData, item0.getHeaderHeight() + 2 * MARGIN);
+				parent.layout(true, true);
 				getVElement().setCollapsed(true);
 			}
 
 			@Override
 			public void itemExpanded(ExpandEvent e) {
+				item0.setHeight(computeHeight(composite));
+				final Object layoutData = bar.getLayoutData();
+				updateLayoutData(layoutData, computeHeight(composite) + item0.getHeaderHeight()
+					+ 2 * MARGIN);
+				parent.layout(true, true);
 				getVElement().setCollapsed(false);
 			}
 
+			// XXX relayout upon expand/collapse will only work properly when the grid data is adjusted
+			// this might not be the case for non GridData layout datas
+			private void updateLayoutData(final Object layoutData, int height) {
+				if (layoutData instanceof GridData) {
+					final GridData gridData = (GridData) layoutData;
+					gridData.heightHint = height;
+				}
+			}
+
 		});
-		item0.setExpanded(getVElement().isCollapsed());
+		item0.setExpanded(!getVElement().isCollapsed());
 		return bar;
 	}
 
+	private int computeHeight(Composite composite) {
+		// XXX +1 because last pixel gets cut off on windows 7 64
+		return composite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 1;
+	}
 }
