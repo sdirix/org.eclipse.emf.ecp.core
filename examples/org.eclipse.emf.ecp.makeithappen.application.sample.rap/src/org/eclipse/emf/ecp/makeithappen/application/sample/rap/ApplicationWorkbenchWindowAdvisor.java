f1e328912852a2a13bf13051bfcd97d1759ca9f5
abuzila <alexandra.buzila@gmail.com>
2014-07-22 17:29:50 +0200

    Bug 439976 - Create ExampleWizard for RAP https://bugs.eclipse.org/bugs/show_bug.cgi?id=439976
    - fixed target + launchin example
    
    Change-Id: Ia9f8dbc6e89c208b8f499f92cbf7490130c9d1d5
    Signed-off-by: eneufeld <neufeld.eugen@googlemail.com>

---
+++ examples/org.eclipse.emf.ecp.makeithappen.application.sample.rap/src/org/eclipse/emf/ecp/makeithappen/application/sample/rap/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,29 @@
+package org.eclipse.emf.ecp.makeithappen.application.sample.rap;
+
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(
+		IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	@Override
+	public void preWindowOpen() {
+		final IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(500, 750));
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(false);
+		configurer.setTitle("MakeItHappen ECP Sample RAP Application"); //$NON-NLS-1$
+	}
+}
