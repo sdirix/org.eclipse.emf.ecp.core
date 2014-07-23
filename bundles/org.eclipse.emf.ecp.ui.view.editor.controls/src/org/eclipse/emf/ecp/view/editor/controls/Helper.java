c3173cad40fe3bad1b192414a434f839f1339b95
abuzila <alexandra.buzila@gmail.com>
2014-07-23 16:10:25 +0200

    Bug 438874 - [View Editor] Selecting feature coming from an abstract EClass throws exception https://bugs.eclipse.org/bugs/show_bug.cgi?id=438874
    Change-Id: I2bb1249d8164863b7289dbe972dd28cb07784273
    Signed-off-by: abuzila <alexandra.buzila@gmail.com>

--- bundles/org.eclipse.emf.ecp.ui.view.editor.controls/src/org/eclipse/emf/ecp/view/editor/controls/Helper.java
+++ bundles/org.eclipse.emf.ecp.ui.view.editor.controls/src/org/eclipse/emf/ecp/view/editor/controls/Helper.java
@@ -179,10 +179,12 @@ public final class Helper {
 
 		EClass eClassToCheck = eClass;
 		final EStructuralFeature featureToCheck = (EStructuralFeature) treePath.getLastSegment();
-		for (int i = 0; i < treePath.getSegmentCount() - 1; i++) {
-			if (EReference.class.isInstance(treePath.getSegment(i))) {
-				eClassToCheck = EReference.class.cast(treePath.getSegment(i)).getEReferenceType();
-			}
+		final int segments = treePath.getSegmentCount();
+		if (segments > 1 && EReference.class.isInstance(treePath.getSegment(segments - 1))) {
+			eClassToCheck = EReference.class.cast(treePath.getSegment(segments - 1)).getEReferenceType();
+		}
+		if (eClassToCheck.isAbstract() || eClass.isInterface()) {
+			return false;
 		}
 
 		final ComposedAdapterFactory composedAdapterFactory = new ComposedAdapterFactory(new AdapterFactory[] {
@@ -190,6 +192,7 @@ public final class Helper {
 			new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE) });
 		final AdapterFactoryItemDelegator adapterFactoryItemDelegator = new AdapterFactoryItemDelegator(
 			composedAdapterFactory);
+
 		final IItemPropertyDescriptor propertyDescriptor =
 			adapterFactoryItemDelegator
 				.getPropertyDescriptor(EcoreUtil.create(eClassToCheck), featureToCheck);
