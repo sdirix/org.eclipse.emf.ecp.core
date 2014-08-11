cf0f98f124fa381706eae8e67fff79598e246972
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2014-08-11 10:34:47 +0200

    Bug 440840 - Default Renderer for Compound Control view model https://bugs.eclipse.org/bugs/show_bug.cgi?id=440840
    Change-Id: Iaa6ed5ed24b467a5917d9f0f6769e196e1a8f48d

--- bundles/org.eclipse.emf.ecp.view.compoundcontrol.swt/src/org/eclipse/emf/ecp/view/spi/compoundcontrol/swt/CompoundControlSWTRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.compoundcontrol.swt/src/org/eclipse/emf/ecp/view/spi/compoundcontrol/swt/CompoundControlSWTRenderer.java
@@ -11,80 +11,132 @@
  */
 package org.eclipse.emf.ecp.view.spi.compoundcontrol.swt;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecp.view.internal.compoundcontrol.swt.Activator;
 import org.eclipse.emf.ecp.view.spi.compoundcontrol.model.VCompoundControl;
-import org.eclipse.emf.ecp.view.spi.core.swt.ContainerSWTRenderer;
+import org.eclipse.emf.ecp.view.spi.model.LabelAlignment;
 import org.eclipse.emf.ecp.view.spi.model.VContainedElement;
-import org.eclipse.emf.ecp.view.spi.swt.SWTRendererFactory;
+import org.eclipse.emf.ecp.view.spi.model.VControl;
+import org.eclipse.emf.ecp.view.spi.model.VElement;
+import org.eclipse.emf.ecp.view.spi.renderer.NoPropertyDescriptorFoundExeption;
+import org.eclipse.emf.ecp.view.spi.renderer.NoRendererFoundException;
+import org.eclipse.emf.ecp.view.spi.swt.AbstractSWTRenderer;
+import org.eclipse.emf.ecp.view.spi.swt.layout.GridDescriptionFactory;
+import org.eclipse.emf.ecp.view.spi.swt.layout.LayoutProviderHelper;
+import org.eclipse.emf.ecp.view.spi.swt.layout.SWTGridCell;
+import org.eclipse.emf.ecp.view.spi.swt.layout.SWTGridDescription;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Control;
 
 /**
- * {@link ContainerSWTRenderer} for the {@link VCompoundControl} view model. Basically a group renderer.
+ * {@link AbstractSWTRenderer} for the {@link VCompoundControl} view model.
  *
  * @author jfaltermeier
  *
  */
-public class CompoundControlSWTRenderer extends ContainerSWTRenderer<VCompoundControl> {
+public class CompoundControlSWTRenderer extends AbstractSWTRenderer<VCompoundControl> {
+
 	private static final String COMPOUND_CONTROL = "org_eclipse_emf_ecp_ui_compound_control"; //$NON-NLS-1$
+	private SWTGridDescription rendererGridDescription;
 
 	/**
-	 * Default constructor.
-	 */
-	public CompoundControlSWTRenderer() {
-		super();
-	}
-
-	/**
-	 * Test constructor.
+	 * {@inheritDoc}
 	 *
-	 * @param factory the {@link SWTRendererFactory} to use.
+	 * @see org.eclipse.emf.ecp.view.spi.swt.AbstractSWTRenderer#dispose()
 	 */
-	protected CompoundControlSWTRenderer(SWTRendererFactory factory) {
-		super(factory);
+	@Override
+	protected void dispose() {
+		rendererGridDescription = null;
+		super.dispose();
 	}
 
 	/**
 	 * {@inheritDoc}
 	 *
-	 * @see org.eclipse.emf.ecp.view.spi.core.swt.ContainerSWTRenderer#getCustomVariant()
+	 * @see org.eclipse.emf.ecp.view.spi.swt.AbstractSWTRenderer#getGridDescription(SWTGridDescription)
 	 */
 	@Override
-	protected String getCustomVariant() {
-		return COMPOUND_CONTROL;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see org.eclipse.emf.ecp.view.spi.core.swt.ContainerSWTRenderer#getChildren()
-	 */
-
-	@SuppressWarnings("restriction")
-	@Override
-	protected Collection<VContainedElement> getChildren() {
-		final List<VContainedElement> children = new ArrayList<VContainedElement>();
-		children.addAll(getVElement().getControls());
-		return children;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see org.eclipse.emf.ecp.view.spi.core.swt.ContainerSWTRenderer#getComposite(org.eclipse.swt.widgets.Composite)
-	 */
-	@SuppressWarnings("restriction")
-	@Override
-	protected Composite getComposite(Composite parent) {
-		final Group group = new Group(parent, SWT.TITLE);
-		if (getVElement().getName() != null) {
-			group.setText(getVElement().getName());
+	public SWTGridDescription getGridDescription(SWTGridDescription gridDescription) {
+		if (rendererGridDescription == null) {
+			rendererGridDescription = GridDescriptionFactory.INSTANCE.createSimpleGrid(1, 1, this);
 		}
-		return group;
+		return rendererGridDescription;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emf.ecp.view.spi.swt.AbstractSWTRenderer#renderControl(int, org.eclipse.swt.widgets.Composite,
+	 *      org.eclipse.emf.ecp.view.spi.model.VElement, org.eclipse.emf.ecp.view.spi.context.ViewModelContext)
+	 */
+	@SuppressWarnings("restriction")
+	@Override
+	protected Control renderControl(SWTGridCell gridCell, Composite parent) throws NoRendererFoundException,
+	NoPropertyDescriptorFoundExeption {
+		if (gridCell.getColumn() != 0) {
+			return null;
+		}
+		final Composite columnComposite = new Composite(parent, SWT.NONE);
+		columnComposite.setBackground(parent.getBackground());
+		columnComposite.setData(CUSTOM_VARIANT, COMPOUND_CONTROL);
+
+		final Map<VContainedElement, AbstractSWTRenderer<VElement>> elementRendererMap = new LinkedHashMap<VContainedElement, AbstractSWTRenderer<VElement>>();
+		for (final VControl child : getVElement().getControls()) {
+
+			child.setLabelAlignment(LabelAlignment.NONE);
+			final AbstractSWTRenderer<VElement> renderer = getSWTRendererFactory().getRenderer(child,
+				getViewModelContext());
+			if (renderer == null) {
+				Activator
+				.getDefault()
+				.getLog()
+				.log(
+					new Status(IStatus.INFO, Activator.PLUGIN_ID, String.format(
+						"No Renderer for %s found.", child.eClass().getName()))); //$NON-NLS-1$
+				continue;
+			}
+			elementRendererMap.put(child, renderer);
+		}
+		// Gridlayout does not have an overflow as other Layouts might have.
+
+		columnComposite.setLayout(LayoutProviderHelper.getColumnLayout(elementRendererMap.size(), true));
+		for (final VContainedElement child : elementRendererMap.keySet()) {
+			final AbstractSWTRenderer<VElement> renderer = elementRendererMap.get(child);
+			final Composite column = new Composite(columnComposite, SWT.NONE);
+			column.setBackground(parent.getBackground());
+
+			column.setLayoutData(LayoutProviderHelper.getSpanningLayoutData(1, 1));
+
+			final SWTGridDescription gridDescription = renderer.getGridDescription(GridDescriptionFactory.INSTANCE
+				.createEmptyGridDescription());
+			column.setLayout(LayoutProviderHelper.getColumnLayout(gridDescription.getColumns(), false));
+
+			try {
+				for (final SWTGridCell childGridCell : gridDescription.getGrid()) {
+					final Control control = childGridCell.getRenderer().render(childGridCell, column);
+					if (control == null) {
+						continue;
+					}
+					// TODO who should apply the layout
+					control.setLayoutData(LayoutProviderHelper.getLayoutData(childGridCell, gridDescription,
+						gridDescription,
+						gridDescription, childGridCell.getRenderer().getVElement(),
+						control));
+				}
+				for (final SWTGridCell childGridCell : gridDescription.getGrid()) {
+					childGridCell.getRenderer().finalizeRendering(column);
+				}
+			} catch (final NoPropertyDescriptorFoundExeption e) {
+				continue;
+			}
+		}
+
+		return columnComposite;
 	}
 
 }
