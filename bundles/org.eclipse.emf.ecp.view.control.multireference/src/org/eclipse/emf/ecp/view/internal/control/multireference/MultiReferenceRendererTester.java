ea1f0db895d209a513f7c3ec22534ed77334d6f8
eneufeld <neufeld.eugen@googlemail.com>
2014-08-19 18:08:17 +0200

    Bug 441796 - Add more stable MultiReference Control https://bugs.eclipse.org/bugs/show_bug.cgi?id=441796

--- bundles/org.eclipse.emf.ecp.view.control.multireference/src/org/eclipse/emf/ecp/view/internal/control/multireference/MultiReferenceRendererTester.java
+++ bundles/org.eclipse.emf.ecp.view.control.multireference/src/org/eclipse/emf/ecp/view/internal/control/multireference/MultiReferenceRendererTester.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2011-2014 EclipseSource Muenchen GmbH and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Eugen Neufeld - initial API and implementation
  ******************************************************************************/
@@ -23,11 +23,10 @@ import org.eclipse.emf.ecp.view.spi.model.VElement;
 
 /**
  * Tester for MultiReference Control.
- * 
+ *
  * @author Eugen Neufeld
- * 
+ *
  */
-@SuppressWarnings("restriction")
 public class MultiReferenceRendererTester implements ECPRendererTester {
 
 	@Override
