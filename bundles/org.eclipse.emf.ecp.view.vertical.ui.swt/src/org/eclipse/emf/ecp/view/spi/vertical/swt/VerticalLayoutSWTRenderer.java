a63f279890263218a886289e298c1608ad39ef64
jonas <jonas.helming@googlemail.com>
2014-10-06 10:57:17 +0200

    Bug 412348 - Remove all warnings

--- bundles/org.eclipse.emf.ecp.view.vertical.ui.swt/src/org/eclipse/emf/ecp/view/spi/vertical/swt/VerticalLayoutSWTRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.vertical.ui.swt/src/org/eclipse/emf/ecp/view/spi/vertical/swt/VerticalLayoutSWTRenderer.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2011-2013 EclipseSource Muenchen GmbH and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Edagr Mueller - initial API and implementation
  * Eugen Neufeld - Refactoring
@@ -20,19 +20,18 @@ import org.eclipse.emf.ecp.view.spi.vertical.model.VVerticalLayout;
 
 /**
  * The SWT Renderer for the {@link VVerticalLayout}. It renders all elements under each other.
- * 
+ *
  * @author Eugen Neufeld
  * @since 1.2
- * 
+ *
  */
-@SuppressWarnings("restriction")
 public class VerticalLayoutSWTRenderer extends ContainerSWTRenderer<VVerticalLayout> {
 
 	private static final String CONTROL_COLUMN = "org_eclipse_emf_ecp_ui_layout_vertical"; //$NON-NLS-1$
 
 	/**
 	 * {@inheritDoc}
-	 * 
+	 *
 	 * @see org.eclipse.emf.ecp.view.spi.core.swt.ContainerSWTRenderer#getChildren()
 	 */
 	@Override
@@ -42,7 +41,7 @@ public class VerticalLayoutSWTRenderer extends ContainerSWTRenderer<VVerticalLay
 
 	/**
 	 * {@inheritDoc}
-	 * 
+	 *
 	 * @see org.eclipse.emf.ecp.view.spi.core.swt.ContainerSWTRenderer#getCustomVariant()
 	 */
 	@Override
