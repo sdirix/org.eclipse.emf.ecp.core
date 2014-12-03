c8bbe1bf1eb9490b35425e4cd67610ffe6b56ead
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2014-12-03 13:59:48 +0100

    Bug 453435 - Split plugin and unit tests * Renamed test https://bugs.eclipse.org/bugs/show_bug.cgi?id=453435

--- tests/org.eclipse.emf.ecp.ecore.editor.test/src/org/eclipse/emf/ecp/ecore/editor/test/EcoreGenModelLinker_PTest.java
+++ tests/org.eclipse.emf.ecp.ecore.editor.test/src/org/eclipse/emf/ecp/ecore/editor/test/EcoreGenModelLinker_PTest.java
@@ -23,7 +23,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-public class EcoreGenModelLinker_Test {
+public class EcoreGenModelLinker_PTest {
 
 	// JUnit rule needs to be public.
 	// REUSED CLASS
