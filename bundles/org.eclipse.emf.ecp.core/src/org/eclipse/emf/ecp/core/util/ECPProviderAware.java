9e86caa7fdce3b710a7371243280fd10c4f25df1
jonas <jonas.helming@googlemail.com>
2014-12-10 14:35:40 +0100

    Bug 454724 - ECPProviderAware is marked as noimplement, but provider need to implement it

--- bundles/org.eclipse.emf.ecp.core/src/org/eclipse/emf/ecp/core/util/ECPProviderAware.java
+++ bundles/org.eclipse.emf.ecp.core/src/org/eclipse/emf/ecp/core/util/ECPProviderAware.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2011-2013 EclipseSource Muenchen GmbH and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Eike Stepper - initial API and implementation
  * Eugen Neufeld - JavaDoc
- * 
+ *
  *******************************************************************************/
 package org.eclipse.emf.ecp.core.util;
 
@@ -17,16 +17,14 @@ import org.eclipse.emf.ecp.core.ECPProvider;
 
 /**
  * This interface is used to define classes that know the provider they belong to.
- * 
+ *
  * @author Eike Stepper
  * @author Eugen Neufeld
- * @noextend This interface is not intended to be extended by clients.
- * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ECPProviderAware {
 	/**
 	 * The Provider this class references in any way.
-	 * 
+	 *
 	 * @return the known {@link ECPProvider}
 	 */
 	ECPProvider getProvider();
