6743cae5136385cb9206e6a925652987fac483bf
eneufeld <neufeld.eugen@googlemail.com>
2015-02-25 17:02:32 +0100

    Bug 460818 - Provide central LocalizationHelper for dynamic language switch https://bugs.eclipse.org/bugs/show_bug.cgi?id=460818
    Change-Id: If565397b0aaf521c53c46b4df063809eae5b658a
    Signed-off-by: eneufeld <neufeld.eugen@googlemail.com>

---
+++ bundles/org.eclipse.emf.emfforms.localization/src/org/eclipse/emf/emfforms/spi/localization/LocalizationServiceHelper.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Eugen Neufeld - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.emfforms.spi.localization;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.eclipse.osgi.service.localization.BundleLocalization;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * Helper class for retrieving translated strings.
+ *
+ * @author Eugen Neufeld
+ */
+public final class LocalizationServiceHelper {
+
+	private static LocalizationServiceHelper instance;
+
+	private static LocalizationServiceHelper getInstance() {
+		if (instance == null) {
+			instance = new LocalizationServiceHelper();
+		}
+		return instance;
+	}
+
+	private final BundleContext bundleContext;
+
+	private LocalizationServiceHelper() {
+		bundleContext = FrameworkUtil.getBundle(LocalizationServiceHelper.class)
+			.getBundleContext();
+	}
+
+	private String getLocale() {
+		final ServiceReference<EMFFormsLocaleProvider> serviceReference = bundleContext
+			.getServiceReference(EMFFormsLocaleProvider.class);
+		if (serviceReference == null) {
+			return null;
+		}
+		final EMFFormsLocaleProvider localeProvider = bundleContext.getService(serviceReference);
+		final Locale result = localeProvider.getLocale();
+		bundleContext.ungetService(serviceReference);
+		return result.getLanguage();
+	}
+
+	private String getString(Bundle bundle, String key) {
+		return getString(bundle, getLocale(), key);
+	}
+
+	private String getString(Bundle bundle, String localeLanguage, String key) {
+		final ServiceReference<BundleLocalization> serviceReference = bundleContext
+			.getServiceReference(BundleLocalization.class);
+		final BundleLocalization bundleLocalization = bundleContext.getService(serviceReference);
+		final ResourceBundle resourceBundle = bundleLocalization.getLocalization(bundle, localeLanguage);
+		final String result = resourceBundle.getString(key);
+		bundleContext.ungetService(serviceReference);
+		return result;
+	}
+
+	/**
+	 * Return the String for the provided key.
+	 *
+	 * @param clazz The class which needs a translated string
+	 * @param key The key of the string
+	 * @return The translated key
+	 */
+	public static String getString(Class<?> clazz, String key) {
+		return getInstance().getString(FrameworkUtil.getBundle(clazz), key);
+	}
+}
