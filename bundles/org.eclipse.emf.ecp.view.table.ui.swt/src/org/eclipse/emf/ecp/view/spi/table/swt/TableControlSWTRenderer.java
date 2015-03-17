51f82e8df52a7777bb4ffdcd38fdb3c0ddcbd1db
Alexandra Buzila <abuzila@eclipsesource.com>
2015-03-17 15:51:58 +0100

    Bug 453533 - table control labels do not show tooltip https://bugs.eclipse.org/bugs/show_bug.cgi?id=453533
    Change-Id: I5f710d5f4b472f35ce1c45918290a118ce425354
    Signed-off-by: Alexandra Buzila <abuzila@eclipsesource.com>

--- bundles/org.eclipse.emf.ecp.view.table.ui.swt/src/org/eclipse/emf/ecp/view/spi/table/swt/TableControlSWTRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.table.ui.swt/src/org/eclipse/emf/ecp/view/spi/table/swt/TableControlSWTRenderer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011-2013 EclipseSource Muenchen GmbH and others.
+ * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -219,6 +219,8 @@ public class TableControlSWTRenderer extends AbstractControlSWTRenderer<VTableCo
 		final String labelText = labelService.getDisplayName(dmrToCheck,
 			getViewModelContext().getDomainModel());
 		label.setText(labelText);
+		final String labelTooltipText = labelService.getDescription(dmrToCheck, getViewModelContext().getDomainModel());
+		label.setToolTipText(labelTooltipText);
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.BEGINNING).grab(true, false).applyTo(label);
 
 		// VALIDATION
