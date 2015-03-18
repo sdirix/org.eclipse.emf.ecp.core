e438fa094dd498f580b75cd89065fdfb8af80377
Lucas Koehler <l.koehler93@gmail.com>
2015-03-18 11:24:15 +0100

    Bug 461481 - Remove Iterator and ItemPropertyDescriptor Usage https://bugs.eclipse.org/bugs/show_bug.cgi?id=461481
    locally fixed SWTBot test cases except CellReadOnly_PTest
    
    Change-Id: I1ee5cffc137fb8fbd754a5bcb56912196ba2cd36
    Signed-off-by: Lucas Koehler <l.koehler93@gmail.com>

---
+++ tests/org.eclipse.emf.ecp.view.dynamictree.model/src/org/eclipse/emf/ecp/view/dynamictree/model/impl/DynamicContainmentTreeDMRConverter.java
@@ -0,0 +1,176 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Lucas Koehler - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.ecp.view.dynamictree.model.impl;
+
+import org.eclipse.core.databinding.property.list.IListProperty;
+import org.eclipse.core.databinding.property.value.IValueProperty;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecp.view.dynamictree.model.DynamicContainmentItem;
+import org.eclipse.emf.ecp.view.dynamictree.model.DynamicContainmentTreeDomainModelReference;
+import org.eclipse.emf.ecp.view.spi.model.VDomainModelReference;
+import org.eclipse.emfforms.spi.core.services.databinding.DatabindingFailedException;
+import org.eclipse.emfforms.spi.core.services.databinding.DomainModelReferenceConverter;
+import org.eclipse.emfforms.spi.core.services.databinding.EMFFormsDatabinding;
+
+/**
+ * A {@link DomainModelReferenceConverter} that converts a {@link DynamicContainmentTreeDomainModelReference} to an
+ * {@link IListProperty IListProperty} or an {@link IValueProperty}.
+ *
+ * @author Lucas Koehler
+ *
+ */
+public class DynamicContainmentTreeDMRConverter implements DomainModelReferenceConverter {
+
+	private EMFFormsDatabinding emfFormsDatabinding;
+
+	/**
+	 * Sets the {@link EMFFormsDatabinding}.
+	 *
+	 * @param emfFormsDatabinding the emfFormsDatabinding to set
+	 */
+	protected void setEMFFormsDatabinding(EMFFormsDatabinding emfFormsDatabinding) {
+		this.emfFormsDatabinding = emfFormsDatabinding;
+	}
+
+	/**
+	 * Unsets the {@link EMFFormsDatabinding}.
+	 *
+	 * @param emfFormsDatabinding the emfFormsDatabinding to unset
+	 */
+	protected void unsetEMFFormsDatabinding(EMFFormsDatabinding emfFormsDatabinding) {
+		this.emfFormsDatabinding = null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.core.services.databinding.DomainModelReferenceConverter#isApplicable(org.eclipse.emf.ecp.view.spi.model.VDomainModelReference)
+	 */
+	@Override
+	public double isApplicable(VDomainModelReference domainModelReference) {
+		if (domainModelReference == null) {
+			throw new IllegalArgumentException("The given VDomainModelReference must not be null."); //$NON-NLS-1$
+		}
+		if (domainModelReference instanceof DynamicContainmentTreeDomainModelReference) {
+			return 10d;
+		}
+		return NOT_APPLICABLE;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.core.services.databinding.DomainModelReferenceConverter#convertToValueProperty(org.eclipse.emf.ecp.view.spi.model.VDomainModelReference)
+	 */
+	@Override
+	public IValueProperty convertToValueProperty(VDomainModelReference domainModelReference)
+		throws DatabindingFailedException {
+		final DynamicContainmentTreeDomainModelReference dynamicContainmentTreeReference = getAndCheckDynamicContainmentTreeDMR(domainModelReference);
+
+		final int index = getIndex(dynamicContainmentTreeReference);
+
+		final EMFIndexedValuePropertyDelegator indexedProperty = getIndexedRootProperty(
+			dynamicContainmentTreeReference, index);
+		final IValueProperty valuePropertyFromBase = emfFormsDatabinding
+			.getValueProperty(dynamicContainmentTreeReference.getPathFromBase());
+
+		return indexedProperty.value(valuePropertyFromBase);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.core.services.databinding.DomainModelReferenceConverter#convertToListProperty(org.eclipse.emf.ecp.view.spi.model.VDomainModelReference)
+	 */
+	@Override
+	public IListProperty convertToListProperty(VDomainModelReference domainModelReference)
+		throws DatabindingFailedException {
+		final DynamicContainmentTreeDomainModelReference dynamicContainmentTreeReference = getAndCheckDynamicContainmentTreeDMR(domainModelReference);
+
+		final int index = getIndex(dynamicContainmentTreeReference);
+
+		final EMFIndexedValuePropertyDelegator indexedProperty = getIndexedRootProperty(
+			dynamicContainmentTreeReference, index);
+		final IListProperty listPropertyFromBase = emfFormsDatabinding
+			.getListProperty(dynamicContainmentTreeReference.getPathFromBase());
+
+		return indexedProperty.list(listPropertyFromBase);
+	}
+
+	private EMFIndexedValuePropertyDelegator getIndexedRootProperty(
+		final DynamicContainmentTreeDomainModelReference dynamicContainmentTreeReference, final int index)
+		throws DatabindingFailedException, IllegalListTypeException {
+		final IValueProperty valuePropertyFromRoot = emfFormsDatabinding
+			.getValueProperty(dynamicContainmentTreeReference.getPathFromRoot());
+		final EStructuralFeature structuralFeature = (EStructuralFeature) valuePropertyFromRoot.getValueType();
+		checkListType(structuralFeature);
+
+		final EMFIndexedValuePropertyDelegator indexedProperty = new EMFIndexedValuePropertyDelegator(
+			valuePropertyFromRoot, index);
+		return indexedProperty;
+	}
+
+	private DynamicContainmentTreeDomainModelReference getAndCheckDynamicContainmentTreeDMR(
+		VDomainModelReference domainModelReference) throws DatabindingFailedException {
+		if (domainModelReference == null) {
+			throw new IllegalArgumentException("The given VDomainModelReference must not be null."); //$NON-NLS-1$
+		}
+		if (!DynamicContainmentTreeDomainModelReference.class.isInstance(domainModelReference)) {
+			throw new IllegalArgumentException(
+				"DomainModelReference must be an instance of DynamicContainmentTreeDomainModelReference."); //$NON-NLS-1$
+		}
+
+		final DynamicContainmentTreeDomainModelReference dynamicContainmentTreeReference = DynamicContainmentTreeDomainModelReference.class
+			.cast(domainModelReference);
+		if (dynamicContainmentTreeReference.getPathFromRoot() == null) {
+			throw new DatabindingFailedException(
+				"The field pathFromRoot of the given DynamicContainmentTreeDomainModelReference must not be null."); //$NON-NLS-1$
+		}
+		if (dynamicContainmentTreeReference.getPathFromBase() == null) {
+			throw new DatabindingFailedException(
+				"The field pathFromBase of the given DynamicContainmentTreeDomainModelReference must not be null."); //$NON-NLS-1$
+		}
+		return dynamicContainmentTreeReference;
+	}
+
+	private int getIndex(DynamicContainmentTreeDomainModelReference reference) throws DatabindingFailedException {
+		EObject parent = reference.eContainer();
+		while (!DynamicContainmentItem.class.isInstance(parent) && parent != null) {
+			parent = parent.eContainer();
+		}
+		if (parent == null) {
+			throw new DatabindingFailedException(
+				"The base item index of the DynamicContainmentTreeDomainModelReference could not be resolved because its container is null."); //$NON-NLS-1$
+		}
+		final DynamicContainmentItem packingItem = (DynamicContainmentItem) parent;
+		return packingItem.getBaseItemIndex();
+	}
+
+	/**
+	 * Checks whether the given structural feature references a proper list to generate a value or list property.
+	 *
+	 * @param structuralFeature The feature to check
+	 * @throws IllegalListTypeException if the structural feature doesn't reference a proper list.
+	 */
+	private void checkListType(EStructuralFeature structuralFeature) throws IllegalListTypeException {
+		if (!structuralFeature.isMany()) {
+			throw new IllegalListTypeException(
+				"The DynamicContainmentTreeDomainModelReference's base feature must reference a list."); //$NON-NLS-1$
+		}
+		if (!EReference.class.isInstance(structuralFeature)) {
+			throw new IllegalListTypeException(
+				"The DynamicContainmentTreeDomainModelReference's base feature must reference a list of EObjects."); //$NON-NLS-1$
+		}
+	}
+}
