49a01b6198592f01481de5f66256671d261392fb
eneufeld <eneufeld@eclipsesource.com>
2015-05-11 09:36:55 +0200

    Bug 463275 - Fix Childe Contribution tests in View model editor https://bugs.eclipse.org/bugs/show_bug.cgi?id=463275
    Change-Id: Idb1517db83aa94f2c2e08e19281007d6585a9f86
    Signed-off-by: eneufeld <eneufeld@eclipsesource.com>

--- tests/org.eclipse.emf.ecp.view.model.integrationtest/src/org/eclipse/emf/ecp/view/model/integrationtest/ChildrenDescriptorExtension_PTest.java
+++ tests/org.eclipse.emf.ecp.view.model.integrationtest/src/org/eclipse/emf/ecp/view/model/integrationtest/ChildrenDescriptorExtension_PTest.java
@@ -22,15 +22,25 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecp.common.spi.ChildrenDescriptorCollector;
 import org.eclipse.emf.ecp.view.spi.categorization.model.VCategorizationPackage;
+import org.eclipse.emf.ecp.view.spi.compoundcontrol.model.VCompoundcontrolPackage;
+import org.eclipse.emf.ecp.view.spi.custom.model.VCustomPackage;
 import org.eclipse.emf.ecp.view.spi.group.model.VGroupPackage;
 import org.eclipse.emf.ecp.view.spi.groupedgrid.model.VGroupedGridPackage;
+import org.eclipse.emf.ecp.view.spi.horizontal.model.VHorizontalPackage;
+import org.eclipse.emf.ecp.view.spi.label.model.VLabelPackage;
 import org.eclipse.emf.ecp.view.spi.model.VViewPackage;
 import org.eclipse.emf.ecp.view.spi.rule.model.RulePackage;
+import org.eclipse.emf.ecp.view.spi.section.model.VSectionPackage;
+import org.eclipse.emf.ecp.view.spi.stack.model.VStackPackage;
 import org.eclipse.emf.ecp.view.spi.table.model.VTablePackage;
+import org.eclipse.emf.ecp.view.spi.vertical.model.VVerticalPackage;
+import org.eclipse.emf.ecp.view.spi.viewproxy.model.VViewproxyPackage;
+import org.eclipse.emf.ecp.view.treemasterdetail.model.VTreeMasterDetailPackage;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.junit.Test;
 
+@SuppressWarnings("restriction")
 public class ChildrenDescriptorExtension_PTest {
 
 	/**
@@ -43,8 +53,11 @@ public class ChildrenDescriptorExtension_PTest {
 	private static final int COMPOSITE_CHILD_COUNT = RENDERABLE_CHILD_COUNT;
 	private static final int ABSTRACTCATEGORIZATION_CHILD_COUNT = RENDERABLE_CHILD_COUNT + 1;
 	private static final int NUMBER_OF_MAIN_COMPOSITES = 1;
-	// label, table, vertical, horizontal, group, groupedgrid, categorizationElement, custom control
-	private static final int NUMBER_OF_EXTERNAL_COMPOSITES = 8;
+	// label, table, vertical, horizontal, group, groupedgrid, categorizationElement, custom control, section,
+	// stack,viewproxy, compoundcontrol, treemasterdetail
+	private static final int NUMBER_OF_EXTERNAL_COMPOSITES = 13;
+	// VDomainModelReference -> VFeaturePathDR, VPredefinedDR, VTableDR,KEYDMR, MAPPINGDMR,IndexDMR
+	private static final int DOMAINMODELREFERENCE_COUNT = 6;
 	private static final int NUMBER_OF_COMPOSITES = NUMBER_OF_MAIN_COMPOSITES + NUMBER_OF_EXTERNAL_COMPOSITES;
 	// categorization, category
 	private static final int NUMBER_OF_CATEGORIZATIONS = 2;
@@ -57,23 +70,19 @@ public class ChildrenDescriptorExtension_PTest {
 	private static final int LEAFCONDITION_CHILD_COUNT = 1;
 	private static final int ORCONDITION_CHILD_COUNT = 3;
 	private static final int ANDCONDITION_CHILD_COUNT = 3;
-	// TODO: Should be not -10, labels, groupedgrid, span, 2 rules, vertical, horizontal, table, group
-	// are missing
+	// TODO: Should be not - NUMBER_OF_EXTERNAL_COMPOSITES
 	// TODO: upper hierarchy is missing, can't find children +2 because of hardcoded layouts
-	private static final int CATEGORY_CHILD_COUNT = NUMBER_OF_COMPOSITES + ABSTRACTCATEGORIZATION_CHILD_COUNT - 10 + 3
-		+ 2;
-	// VDomainModelReference -> VFeaturePathDR, VPredefinedDR, VTableDR
-	private static final int CONTROL_CHILD_COUNT = COMPOSITE_CHILD_COUNT + 3;
+	private static final int CATEGORY_CHILD_COUNT = NUMBER_OF_COMPOSITES + ABSTRACTCATEGORIZATION_CHILD_COUNT
+		- NUMBER_OF_EXTERNAL_COMPOSITES + 2 + 1;
+
+	private static final int CONTROL_CHILD_COUNT = COMPOSITE_CHILD_COUNT + DOMAINMODELREFERENCE_COUNT;
 	private static final int TABLECONTROL_CHILD_COUNT = CONTROL_CHILD_COUNT + 2;
-	// private static final int COLUMNCOMPOSITE_CHILD_COUNT = COMPOSITECOLLECTION_CHILD_COUNT;
-	// private static final int COLUMN_CHILD_COUNT = COMPOSITECOLLECTION_CHILD_COUNT;
-	private static final int GROUP_CHILD_COUNT = COMPOSITECOLLECTION_CHILD_COUNT;
 	private static final int ACTION_CHILD_COUNT = 0;
 	private static final int GROUPEDGRID_CHILD_COUNT = RENDERABLE_CHILD_COUNT + 1;
 	private static final int GRIDEDGROUP_CHILD_COUNT = 1;
-	// TODO: Should be not -7, labels, group, vertical, horizontal, table,categorizationelement are missing
+	// TODO: Should be not - NUMBER_OF_EXTERNAL_COMPOSITES are missing
 	// TODO: upper hierarchy is missing, can't find children
-	private static final int GRIDEDGROUPROW_CHILD_COUNT = NUMBER_OF_COMPOSITES - 7;
+	private static final int GRIDEDGROUPROW_CHILD_COUNT = NUMBER_OF_COMPOSITES - NUMBER_OF_EXTERNAL_COMPOSITES + 1;
 	private static final int GRIDEDGROUPSPAN_CHILD_COUNT = 0;
 
 	private final ChildrenDescriptorCollector descriptorCollector = new ChildrenDescriptorCollector();
@@ -205,7 +214,79 @@ public class ChildrenDescriptorExtension_PTest {
 	@Test
 	public void testGroupDescriptors() {
 		final int size = getChildrenSize(VGroupPackage.eINSTANCE.getGroup());
-		assertEquals(GROUP_CHILD_COUNT, size);
+		assertEquals(COMPOSITECOLLECTION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testHorizontalDescriptors() {
+		final int size = getChildrenSize(VHorizontalPackage.eINSTANCE.getHorizontalLayout());
+		assertEquals(COMPOSITECOLLECTION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testVerticalDescriptors() {
+		final int size = getChildrenSize(VVerticalPackage.eINSTANCE.getVerticalLayout());
+		assertEquals(COMPOSITECOLLECTION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testLabelDescriptors() {
+		final int size = getChildrenSize(VLabelPackage.eINSTANCE.getLabel());
+		// -5 because only the VFeaturePath is found
+		assertEquals(ATTACHMENT_CHILD_COUNT + DOMAINMODELREFERENCE_COUNT - 5, size);
+	}
+
+	@Test
+	public void testCustomControlDescriptors() {
+		final int size = getChildrenSize(VCustomPackage.eINSTANCE.getCustomControl());
+		assertEquals(CONTROL_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testSectionDescriptors() {
+		final int size = getChildrenSize(VSectionPackage.eINSTANCE.getSection());
+		assertEquals(COMPOSITECOLLECTION_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testSectionAreaDescriptors() {
+		final int size = getChildrenSize(VSectionPackage.eINSTANCE.getSectionedArea());
+		// +1 because of section
+		assertEquals(ATTACHMENT_CHILD_COUNT + 1, size);
+	}
+
+	@Test
+	public void testStackItemDescriptors() {
+		final int size = getChildrenSize(VStackPackage.eINSTANCE.getStackItem());
+		// -1 because of treemasterdetail as it only can be added to containedContainer
+		assertEquals(COMPOSITECOLLECTION_CHILD_COUNT - 1, size);
+	}
+
+	@Test
+	public void testStackLayoutDescriptors() {
+		final int size = getChildrenSize(VStackPackage.eINSTANCE.getStackLayout());
+		// +1 for stack items
+		assertEquals(ATTACHMENT_CHILD_COUNT + 1, size);
+	}
+
+	@Test
+	public void testViewProxyDescriptors() {
+		final int size = getChildrenSize(VViewproxyPackage.eINSTANCE.getViewProxy());
+		assertEquals(ATTACHMENT_CHILD_COUNT, size);
+	}
+
+	@Test
+	public void testCompoundControlDescriptors() {
+		final int size = getChildrenSize(VCompoundcontrolPackage.eINSTANCE.getCompoundControl());
+		// +1 because of vcontrol TODO table and custom control is missing
+		assertEquals(ATTACHMENT_CHILD_COUNT + 1, size);
+	}
+
+	@Test
+	public void testTreeMasterDetailDescriptors() {
+		final int size = getChildrenSize(VTreeMasterDetailPackage.eINSTANCE.getTreeMasterDetail());
+		// +1 because of VView
+		assertEquals(ATTACHMENT_CHILD_COUNT + 1, size);
 	}
 
 	/**
