e5e7b175e5b1dff4fb36b360a654e6151ab6264d
Lucas Koehler <l.koehler93@gmail.com>
2015-06-29 11:10:39 +0200

    Bug 470149 - Implement a domain expansion service https://bugs.eclipse.org/bugs/show_bug.cgi?id=470149
    Implement a mapping dmr expander.
    
    Change-Id: Iafc80bcf26a2d804e1a7e9f7fc2fcbd390003e3b
    Signed-off-by: Lucas Koehler <l.koehler93@gmail.com>

---
+++ bundles/org.eclipse.emfforms.core.services.domainexpander.mapping/src/org/eclipse/emfforms/internal/core/services/domainexpander/mapping/EMFFormsMappingDMRExpander.java
@@ -0,0 +1,169 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Lucas Koehler - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emfforms.internal.core.services.domainexpander.mapping;
+
+import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecp.common.spi.asserts.Assert;
+import org.eclipse.emf.ecp.view.spi.mappingdmr.model.VMappingDomainModelReference;
+import org.eclipse.emf.ecp.view.spi.model.VDomainModelReference;
+import org.eclipse.emf.ecp.view.spi.model.VFeaturePathDomainModelReference;
+import org.eclipse.emf.ecp.view.spi.model.VViewFactory;
+import org.eclipse.emfforms.spi.common.report.AbstractReport;
+import org.eclipse.emfforms.spi.common.report.ReportService;
+import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRExpander;
+import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDomainExpander;
+import org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsExpandingFailedException;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * An {@link EMFFormsDMRExpander} for expanding {@link VMappingDomainModelReference VMappingDomainModelReferences}.
+ *
+ * @author Lucas Koehler
+ *
+ */
+@Component(name = "EMFFormsMappingDMRExpander")
+public class EMFFormsMappingDMRExpander implements EMFFormsDMRExpander {
+
+	private ReportService reportService;
+	private EMFFormsDomainExpander domainExpander;
+
+	/**
+	 * Called by the framework to set the {@link ReportService}.
+	 *
+	 * @param reportService The {@link ReportService}
+	 */
+	@Reference
+	protected void setReportService(ReportService reportService) {
+		this.reportService = reportService;
+
+	}
+
+	/**
+	 * Called by the framework to set the {@link EMFFormsDomainExpander}.
+	 *
+	 * @param emfFormsDomainExpander The {@link EMFFormsDomainExpander}
+	 */
+	@Reference
+	protected void setEMFFormsDomainExpander(EMFFormsDomainExpander emfFormsDomainExpander) {
+		domainExpander = emfFormsDomainExpander;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRExpander#prepareDomainObject(org.eclipse.emf.ecp.view.spi.model.VDomainModelReference,
+	 *      org.eclipse.emf.ecore.EObject)
+	 */
+	@Override
+	public void prepareDomainObject(VDomainModelReference domainModelReference, EObject domainObject)
+		throws EMFFormsExpandingFailedException {
+
+		Assert.create(domainModelReference).notNull();
+		Assert.create(domainObject).notNull();
+		Assert.create(domainModelReference).ofClass(VMappingDomainModelReference.class);
+
+		final VMappingDomainModelReference mappingReference = (VMappingDomainModelReference) domainModelReference;
+
+		if (mappingReference.getDomainModelEFeature() == null) {
+			throw new EMFFormsExpandingFailedException(
+				"The mapping domain model reference's domain model e feature must not be null."); //$NON-NLS-1$
+		}
+		checkMapType(mappingReference.getDomainModelEFeature());
+
+		// expand first part of the mapping dmr. A new reference needs to be created because we only want to expand the
+		// first part of the mapping dmr in this step.
+		final VFeaturePathDomainModelReference firstReference = VViewFactory.eINSTANCE
+			.createFeaturePathDomainModelReference();
+		firstReference.setDomainModelEFeature(mappingReference.getDomainModelEFeature());
+		firstReference.getDomainModelEReferencePath().addAll(mappingReference.getDomainModelEReferencePath());
+		domainExpander.prepareDomainObject(firstReference, domainObject);
+
+		// get the EObject that contains the map
+		EObject eObject = domainObject;
+		for (final EReference eRef : firstReference.getDomainModelEReferencePath()) {
+			eObject = (EObject) domainObject.eGet(eRef);
+		}
+
+		// Need unchecked conversion because EObject#eGet always returns an Object
+		@SuppressWarnings("unchecked")
+		final EMap<EClass, EObject> map = (EMap<EClass, EObject>) eObject.eGet(mappingReference
+			.getDomainModelEFeature());
+		if (!map.containsKey(mappingReference.getMappedClass())) {
+			map.put(mappingReference.getMappedClass(), EcoreUtil.create(mappingReference.getMappedClass()));
+		}
+		final EObject mappedEObject = map.get(mappingReference.getMappedClass());
+
+		// expand second part of the mapping dmr
+		domainExpander.prepareDomainObject(mappingReference.getDomainModelReference(), mappedEObject);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.core.services.domainexpander.EMFFormsDMRExpander#isApplicable(org.eclipse.emf.ecp.view.spi.model.VDomainModelReference)
+	 */
+	@Override
+	public double isApplicable(VDomainModelReference domainModelReference) {
+		if (domainModelReference == null) {
+			reportService.report(new AbstractReport("Warning: The given domain model reference was null.")); //$NON-NLS-1$
+			return NOT_APPLICABLE;
+		}
+		if (VMappingDomainModelReference.class.isInstance(domainModelReference)) {
+			return 5d;
+		}
+		return NOT_APPLICABLE;
+	}
+
+	/**
+	 * Checks whether the given structural feature references a proper map.
+	 *
+	 * @param structuralFeature The feature to check
+	 * @throws EMFFormsExpandingFailedException if the structural feature doesn't reference a proper map.
+	 */
+	private void checkMapType(EStructuralFeature structuralFeature) throws EMFFormsExpandingFailedException {
+		if (!structuralFeature.getEType().getInstanceClassName().equals("java.util.Map$Entry")) { //$NON-NLS-1$
+			throw new EMFFormsExpandingFailedException(
+				"The VMappingDomainModelReference's domainModelEFeature must reference a map."); //$NON-NLS-1$
+		}
+		if (structuralFeature.getLowerBound() != 0 || structuralFeature.getUpperBound() != -1) {
+			throw new EMFFormsExpandingFailedException(
+				"The VMappingDomainModelReference's domainModelEFeature must reference a map."); //$NON-NLS-1$
+		}
+
+		final EClass eClass = (EClass) structuralFeature.getEType();
+		final EStructuralFeature keyFeature = eClass.getEStructuralFeature("key"); //$NON-NLS-1$
+		final EStructuralFeature valueFeature = eClass.getEStructuralFeature("value"); //$NON-NLS-1$
+		if (keyFeature == null || valueFeature == null) {
+			throw new EMFFormsExpandingFailedException(
+				"The VMappingDomainModelReference's domainModelEFeature must reference a map."); //$NON-NLS-1$
+		}
+		if (!EReference.class.isInstance(valueFeature)) {
+			throw new EMFFormsExpandingFailedException(
+				"The values of the map referenced by the VMappingDomainModelReference's domainModelEFeature must be referenced EObjects."); //$NON-NLS-1$
+		}
+		if (!EReference.class.isInstance(keyFeature)) {
+			throw new EMFFormsExpandingFailedException(
+				"The keys of the map referenced by the VMappingDomainModelReference's domainModelEFeature must be referenced EClasses."); //$NON-NLS-1$
+		}
+		if (!EClass.class.isAssignableFrom(((EReference) keyFeature).getEReferenceType().getInstanceClass())) {
+			throw new EMFFormsExpandingFailedException(
+				"The keys of the map referenced by the VMappingDomainModelReference's domainModelEFeature must be referenced EClasses."); //$NON-NLS-1$
+		}
+	}
+
+}
---
+++ tests/org.eclipse.emfforms.core.services.domainexpander.mapping.tests/src/org/eclipse/emfforms/core/services/domainexpander/mapping/tests/AllTests.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Lucas Koehler - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emfforms.core.services.domainexpander.mapping.tests;
+
+import org.eclipse.emfforms.internal.core.services.domainexpander.mapping.EMFFormsMappingDMRExpander_Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * All JUnit test cases for core.services.domainexpander.mapping.
+ *
+ * @author Lucas Koehler
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ EMFFormsMappingDMRExpander_Test.class })
+public class AllTests {
+
+}
