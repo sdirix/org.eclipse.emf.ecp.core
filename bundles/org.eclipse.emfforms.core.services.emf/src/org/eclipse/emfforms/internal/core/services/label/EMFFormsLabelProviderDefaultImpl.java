13a3ce931eadf9788926c2463e2e640ea8d65edc
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2015-08-12 15:14:40 +0200

    Bug 474798 - NullPointerException in EMFFormsLabelProviderDefaultImpl.getDisplayName (144) * Adding null guards in EMFFormsLabelProviderDefaultImpl https://bugs.eclipse.org/bugs/show_bug.cgi?id=474798
    Change-Id: I14e9a08e0e44ef22c5acb502472a18ab78b6779b
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- bundles/org.eclipse.emfforms.core.services.emf/src/org/eclipse/emfforms/internal/core/services/label/EMFFormsLabelProviderDefaultImpl.java
+++ bundles/org.eclipse.emfforms.core.services.emf/src/org/eclipse/emfforms/internal/core/services/label/EMFFormsLabelProviderDefaultImpl.java
@@ -22,6 +22,7 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecp.common.spi.asserts.Assert;
 import org.eclipse.emf.ecp.view.spi.model.VDomainModelReference;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emfforms.spi.common.report.AbstractReport;
 import org.eclipse.emfforms.spi.common.report.ReportService;
 import org.eclipse.emfforms.spi.core.services.databinding.DatabindingFailedException;
 import org.eclipse.emfforms.spi.core.services.databinding.DatabindingFailedReport;
@@ -92,6 +93,10 @@ public class EMFFormsLabelProviderDefaultImpl implements EMFFormsLabelProvider {
 		final EObject tempInstance = EcoreUtil.create(eContainingClass);
 		final IItemPropertyDescriptor itemPropertyDescriptor = emfSpecificService.getIItemPropertyDescriptor(
 			tempInstance, structuralFeature);
+		if (itemPropertyDescriptor == null) {
+			reportMissingPropertyDescriptor(tempInstance, structuralFeature);
+			return getConstantObservableValue(getFallbackLabel(structuralFeature));
+		}
 
 		return getConstantObservableValue(itemPropertyDescriptor.getDisplayName(tempInstance));
 	}
@@ -140,6 +145,10 @@ public class EMFFormsLabelProviderDefaultImpl implements EMFFormsLabelProvider {
 		observableValue.dispose();
 		final IItemPropertyDescriptor itemPropertyDescriptor = emfSpecificService.getIItemPropertyDescriptor(value,
 			structuralFeature);
+		if (itemPropertyDescriptor == null) {
+			reportMissingPropertyDescriptor(value, structuralFeature);
+			return getConstantObservableValue(getFallbackLabel(structuralFeature));
+		}
 
 		return getConstantObservableValue(itemPropertyDescriptor.getDisplayName(value));
 	}
@@ -168,6 +177,10 @@ public class EMFFormsLabelProviderDefaultImpl implements EMFFormsLabelProvider {
 		final EObject tempInstance = EcoreUtil.create(eContainingClass);
 		final IItemPropertyDescriptor itemPropertyDescriptor = emfSpecificService.getIItemPropertyDescriptor(
 			tempInstance, structuralFeature);
+		if (itemPropertyDescriptor == null) {
+			reportMissingPropertyDescriptor(tempInstance, structuralFeature);
+			return getConstantObservableValue(getFallbackLabel(structuralFeature));
+		}
 
 		return getConstantObservableValue(itemPropertyDescriptor.getDescription(tempInstance));
 	}
@@ -198,12 +211,22 @@ public class EMFFormsLabelProviderDefaultImpl implements EMFFormsLabelProvider {
 
 		final IItemPropertyDescriptor itemPropertyDescriptor = emfSpecificService.getIItemPropertyDescriptor(value,
 			structuralFeature);
+		if (itemPropertyDescriptor == null) {
+			reportMissingPropertyDescriptor(value, structuralFeature);
+			return getConstantObservableValue(getFallbackLabel(structuralFeature));
+		}
 
 		return getConstantObservableValue(itemPropertyDescriptor.getDescription(value));
 	}
 
 	private IObservableValue getConstantObservableValue(String value) {
 		return Observables.constantObservableValue(value, String.class);
+	}
+
+	private void reportMissingPropertyDescriptor(final EObject value, final EStructuralFeature structuralFeature) {
+		reportService.report(
+			new AbstractReport(String.format("No IItemPropertyDescriptor for feature %2$s in EClass %1$s found.", //$NON-NLS-1$
+				value.eClass().getName(), structuralFeature.getName())));
 	}
 
 }
