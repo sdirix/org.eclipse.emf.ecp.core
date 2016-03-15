6dacd4fed26b00eef32c7e7e3f982ab50f5085de
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2016-03-15 17:28:14 +0100

    Bug 487109 - [Ecore Editor] Create New Dialog OK Button enablement
    Change-Id: I9197c7c1110b0b3a670839f6c767b1411c622c2b
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- bundles/org.eclipse.emfforms.editor/src/org/eclipse/emfforms/internal/editor/ui/CreateDialog.java
+++ bundles/org.eclipse.emfforms.editor/src/org/eclipse/emfforms/internal/editor/ui/CreateDialog.java
@@ -11,15 +11,21 @@
  ******************************************************************************/
 package org.eclipse.emfforms.internal.editor.ui;
 
+import java.util.Set;
+
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.Diagnostician;
 import org.eclipse.emf.ecp.ui.view.ECPRendererException;
+import org.eclipse.emf.ecp.ui.view.swt.ECPSWTView;
 import org.eclipse.emf.ecp.ui.view.swt.ECPSWTViewRenderer;
 import org.eclipse.emf.ecp.view.spi.model.VViewFactory;
 import org.eclipse.emf.ecp.view.spi.model.VViewModelProperties;
+import org.eclipse.emf.ecp.view.spi.validation.ValidationService;
+import org.eclipse.emf.ecp.view.spi.validation.ViewValidationListener;
 import org.eclipse.emfforms.swt.core.EMFFormsSWTConstants;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
@@ -27,6 +33,7 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
@@ -41,6 +48,7 @@ public class CreateDialog extends Dialog {
 
 	/** The new object. */
 	private final EObject newObject;
+	private ECPSWTView ecpswtView;
 
 	/**
 	 * Instantiates a new dialog.
@@ -95,7 +103,15 @@ public class CreateDialog extends Dialog {
 			final VViewModelProperties properties = VViewFactory.eINSTANCE.createViewModelLoadingProperties();
 			properties.addInheritableProperty(EMFFormsSWTConstants.USE_ON_MODIFY_DATABINDING_KEY,
 				EMFFormsSWTConstants.USE_ON_MODIFY_DATABINDING_VALUE);
-			ECPSWTViewRenderer.INSTANCE.render(emfFormsParent, newObject, properties);
+			ecpswtView = ECPSWTViewRenderer.INSTANCE.render(emfFormsParent, newObject, properties);
+			final ValidationService validationService = ecpswtView.getViewModelContext()
+				.getService(ValidationService.class);
+			validationService.registerValidationListener(new ViewValidationListener() {
+				@Override
+				public void onNewValidation(Set<Diagnostic> validationResults) {
+					updateOKButtonEnablement();
+				}
+			});
 		} catch (final ECPRendererException e) {
 		}
 
@@ -147,5 +163,24 @@ public class CreateDialog extends Dialog {
 	 */
 	public EObject getCreatedInstance() {
 		return newObject;
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		super.createButtonsForButtonBar(parent);
+		updateOKButtonEnablement();
+	}
+
+	private void updateOKButtonEnablement() {
+		final Button button = getButton(IDialogConstants.OK_ID);
+		if (button == null) {
+			return;
+		}
+		if (ecpswtView.getViewModelContext().getViewModel().getDiagnostic()
+			.getHighestSeverity() >= Diagnostic.ERROR) {
+			button.setEnabled(false);
+		} else {
+			button.setEnabled(true);
+		}
 	}
 }
