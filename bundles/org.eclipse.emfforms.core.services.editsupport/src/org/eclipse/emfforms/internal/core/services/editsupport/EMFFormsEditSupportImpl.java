893e596b04a1c546410847cee3180c20e79ccf6f
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-04-29 15:31:34 +0200

    Bug 469705 - EnumComboViewerSWTRenderer does not use EMFFormsLocaleProvider
    Added support for localization of enums
    
    Change-Id: Ic18ccae5434035e511e6060708ed8ad84394a120
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

--- bundles/org.eclipse.emfforms.core.services.editsupport/src/org/eclipse/emfforms/internal/core/services/editsupport/EMFFormsEditSupportImpl.java
+++ bundles/org.eclipse.emfforms.core.services.editsupport/src/org/eclipse/emfforms/internal/core/services/editsupport/EMFFormsEditSupportImpl.java
@@ -13,12 +13,18 @@ package org.eclipse.emfforms.internal.core.services.editsupport;
 
 import org.eclipse.core.databinding.observable.IObserving;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecp.common.spi.asserts.Assert;
 import org.eclipse.emf.ecp.view.spi.model.VDomainModelReference;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emfforms.internal.core.services.label.BundleResolver;
+import org.eclipse.emfforms.internal.core.services.label.BundleResolver.NoBundleFoundException;
+import org.eclipse.emfforms.internal.core.services.label.BundleResolverImpl;
 import org.eclipse.emfforms.spi.common.report.AbstractReport;
 import org.eclipse.emfforms.spi.common.report.ReportService;
 import org.eclipse.emfforms.spi.core.services.databinding.DatabindingFailedException;
@@ -26,6 +32,8 @@ import org.eclipse.emfforms.spi.core.services.databinding.DatabindingFailedRepor
 import org.eclipse.emfforms.spi.core.services.databinding.EMFFormsDatabinding;
 import org.eclipse.emfforms.spi.core.services.editsupport.EMFFormsEditSupport;
 import org.eclipse.emfforms.spi.core.services.emfspecificservice.EMFSpecificService;
+import org.eclipse.emfforms.spi.localization.EMFFormsLocalizationService;
+import org.osgi.framework.Bundle;
 
 /**
  * EMF implementation of {@link EMFFormsEditSupport}.
@@ -33,10 +41,22 @@ import org.eclipse.emfforms.spi.core.services.emfspecificservice.EMFSpecificServ
  * @author Lucas Koehler
  *
  */
+@SuppressWarnings("restriction")
 public class EMFFormsEditSupportImpl implements EMFFormsEditSupport {
 	private EMFFormsDatabinding emfFormsDatabinding;
 	private EMFSpecificService emfSpecificService;
 	private ReportService reportService;
+	private final BundleResolver bundleResolver = new BundleResolverImpl();
+	private EMFFormsLocalizationService emfFormsLocalizationService;
+
+	/**
+	 * Sets the {@link EMFFormsLocalizationService} service.
+	 *
+	 * @param emfFormsLocalizationService The localization service.
+	 */
+	protected void setEMFFormsLocalizationService(EMFFormsLocalizationService emfFormsLocalizationService) {
+		this.emfFormsLocalizationService = emfFormsLocalizationService;
+	}
 
 	/**
 	 * Sets the {@link EMFFormsDatabinding} service.
@@ -157,6 +177,14 @@ public class EMFFormsEditSupportImpl implements EMFFormsEditSupport {
 		final EStructuralFeature structuralFeature = (EStructuralFeature) observableValue.getValueType();
 		observableValue.dispose();
 
+		if (EAttribute.class.isInstance(structuralFeature)
+			&& EcorePackage.eINSTANCE.getEEnum().isInstance(EAttribute.class.cast(structuralFeature).getEType())) {
+			final String result = getEnumLiteral(structuralFeature, element);
+			if (result != null) {
+				return result;
+			}
+		}
+
 		final IItemPropertyDescriptor itemPropertyDescriptor = emfSpecificService.getIItemPropertyDescriptor(value,
 			structuralFeature);
 		if (itemPropertyDescriptor == null) {
@@ -166,6 +194,21 @@ public class EMFFormsEditSupportImpl implements EMFFormsEditSupport {
 		final IItemLabelProvider labelProvider = itemPropertyDescriptor.getLabelProvider(rootObject);
 
 		return labelProvider.getText(element);
+	}
+
+	private static final String LITERAL_NAME = "_UI_%1$s_%2$s_literal"; //$NON-NLS-1$
+
+	private String getEnumLiteral(EStructuralFeature feature, Object element) {
+		final EClass eContainingClass = feature.getEContainingClass();
+		Bundle bundle;
+		try {
+			bundle = bundleResolver.getEditBundle(eContainingClass);
+			final String key = String.format(LITERAL_NAME, feature.getEType().getName(), element);
+			return emfFormsLocalizationService.getString(bundle, key);
+		} catch (final NoBundleFoundException ex) {
+			reportService.report(new AbstractReport(ex));
+		}
+		return null;
 	}
 
 	/**
