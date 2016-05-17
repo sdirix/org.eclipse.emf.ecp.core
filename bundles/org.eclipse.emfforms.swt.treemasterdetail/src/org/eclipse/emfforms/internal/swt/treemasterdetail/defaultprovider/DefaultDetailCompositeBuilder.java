2942b6e02a3d6d5f3645fa29c80b2ab4307ebc08
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-05-17 10:09:58 +0200

    TCI - Added HorizontalScroll to TreeMasterDetail DetailPane
    Change-Id: Ib11ece6eab4df0e2a20c271aabeaf84ce4af7ff0
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

--- bundles/org.eclipse.emfforms.swt.treemasterdetail/src/org/eclipse/emfforms/internal/swt/treemasterdetail/defaultprovider/DefaultDetailCompositeBuilder.java
+++ bundles/org.eclipse.emfforms.swt.treemasterdetail/src/org/eclipse/emfforms/internal/swt/treemasterdetail/defaultprovider/DefaultDetailCompositeBuilder.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * jfaltermeier - initial API and implementation
  ******************************************************************************/
@@ -24,7 +24,7 @@ public final class DefaultDetailCompositeBuilder implements DetailCompositeBuild
 	@Override
 	public Composite createDetailComposite(Composite parent) {
 		final ScrolledComposite detailScrollableComposite = new ScrolledComposite(parent,
-			SWT.V_SCROLL | SWT.BORDER);
+			SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
 		detailScrollableComposite.setExpandHorizontal(true);
 		detailScrollableComposite.setExpandVertical(true);
 		return detailScrollableComposite;
