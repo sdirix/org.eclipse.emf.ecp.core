db6943ca6303052623ee2bc59a2298a00d301ac0
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-07-11 15:41:54 +0200

    TCI - Fixed auto load of ecore in rulerepository editor
    the rule repository editor now loads the ecore references by the view
    model.
    
    Change-Id: I5900ec6a66585322b21bb1232f8b2939b36a873a
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

--- bundles/org.eclipse.emfforms.rulerepository.tooling/src/org/eclipse/emfforms/internal/rulerepository/tooling/RuleRepositoryEditor.java
+++ bundles/org.eclipse.emfforms.rulerepository.tooling/src/org/eclipse/emfforms/internal/rulerepository/tooling/RuleRepositoryEditor.java
@@ -11,16 +11,27 @@
  ******************************************************************************/
 package org.eclipse.emfforms.internal.rulerepository.tooling;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.FeatureMap;
+import org.eclipse.emf.ecore.xml.type.AnyType;
+import org.eclipse.emf.ecp.internal.ide.util.EcoreHelper;
+import org.eclipse.emf.ecp.view.spi.model.VView;
 import org.eclipse.emfforms.internal.editor.toolbaractions.LoadEcoreAction;
 import org.eclipse.emfforms.spi.editor.GenericEditor;
 import org.eclipse.jface.action.Action;
+import org.eclipse.ui.IEditorInput;
 
 /**
  * RuleRepositoryEditor.
- * 
+ *
  * @author Eugen Neufeld
  *
  */
@@ -40,5 +51,78 @@ public class RuleRepositoryEditor extends GenericEditor {
 
 		// result.addAll(readToolbarActions());
 		return result;
+	}
+
+	private void registerEcore(ResourceSet resourceSet) throws IOException {
+		for (final Resource resource : resourceSet.getResources()) {
+			if (resource.getContents().isEmpty()) {
+				continue;
+			}
+			if (!VView.class.isInstance(resource.getContents().get(0))) {
+				continue;
+			}
+			final String ecorePath = getEcorePath(resource);
+			if (ecorePath == null) {
+				return;
+			}
+			EcoreHelper.registerEcore(ecorePath);
+		}
+		// resolve all proxies
+		EcoreUtil.resolveAll(resourceSet);
+	}
+
+	private String getEcorePath(Resource resource) {
+		if (resource == null || resource.getContents().isEmpty()) {
+			return null;
+		}
+		final EObject eObject = resource.getContents().get(0);
+		if (VView.class.isInstance(eObject)) {
+			return VView.class.cast(eObject).getEcorePath();
+		}
+		if (AnyType.class.isInstance(eObject)) {
+			/* view model has older ns uri */
+			final FeatureMap anyAttribute = AnyType.class.cast(eObject).getAnyAttribute();
+			for (int i = 0; i < anyAttribute.size(); i++) {
+				final EStructuralFeature feature = anyAttribute.getEStructuralFeature(i);
+				if ("ecorePath".equals(feature.getName())) { //$NON-NLS-1$
+					return (String) anyAttribute.getValue(i);
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	protected ResourceSet loadResource(IEditorInput editorInput) {
+		final ResourceSet result = super.loadResource(editorInput);
+		try {
+			registerEcore(result);
+		} catch (final IOException ex) {
+			ex.printStackTrace();
+		}
+		return super.loadResource(editorInput);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emfforms.spi.editor.GenericEditor#dispose()
+	 */
+	@Override
+	public void dispose() {
+		for (final Resource resource : getResourceSet().getResources()) {
+			if (resource.getContents().isEmpty()) {
+				continue;
+			}
+			if (!VView.class.isInstance(resource.getContents().get(0))) {
+				continue;
+			}
+			final String ecorePath = getEcorePath(resource);
+			if (ecorePath == null) {
+				return;
+			}
+			EcoreHelper.unregisterEcore(ecorePath);
+		}
+		super.dispose();
 	}
 }
