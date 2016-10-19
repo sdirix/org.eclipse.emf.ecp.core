453b8f0add91a568e3e61964737169a04b8fe8f7
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2016-10-19 14:49:18 +0200

    Bug 506207 - [GenericEditor] Reveal does not work when element was not visible any time before
    Change-Id: I22217adfb671d9aa5fde59a39019964d3ab70044
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- bundles/org.eclipse.emfforms.editor/src/org/eclipse/emfforms/spi/editor/GenericEditor.java
+++ bundles/org.eclipse.emfforms.editor/src/org/eclipse/emfforms/spi/editor/GenericEditor.java
@@ -648,14 +648,19 @@ public class GenericEditor extends EditorPart implements IEditingDomainProvider,
 	 * @since 1.10
 	 */
 	public void reveal(EObject objectToReveal) {
-		while (objectToReveal != null && rootView.getSelectionProvider().testFindItem(objectToReveal) == null) {
+		rootView.getSelectionProvider().refresh();
+
+		while (objectToReveal != null) {
+			rootView.getSelectionProvider().reveal(objectToReveal);
+			if (rootView.getSelectionProvider().testFindItem(objectToReveal) != null) {
+				break;
+			}
 			objectToReveal = objectToReveal.eContainer();
 		}
 		if (objectToReveal == null) {
 			return;
 		}
-		rootView.getSelectionProvider().refresh();
-		rootView.getSelectionProvider().reveal(objectToReveal);
+
 		rootView.setSelection(new StructuredSelection(objectToReveal));
 	}
 
