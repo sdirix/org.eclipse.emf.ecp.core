60be5aefdd60a016c1ea35db7f2dd87133c16334
Eugen Neufeld <eneufeld@eclipsesource.com>
2017-01-27 11:19:10 +0100

    TCI - Added Shell script to manually download eclipse for q7
    Change-Id: I06b71fdab44bfb792dae77c923fa98f9d586ba26
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

---
+++ tests/ECPQ7Tests/EPPTests/download.sh
@@ -0,0 +1,8 @@
+if [ $# -lt 3 ]; then
+	exit
+fi
+AUT_DIR="$1/aut"
+rm -rf ${AUT_DIR}/*
+mkdir -p ${AUT_DIR}
+cd ${AUT_DIR}
+wget -q -O $2_$3.tar.gz https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/$2/$3/eclipse-modeling-$2-$3-linux-gtk-x86_64.tar.gz\&r=1 --proxy=off
