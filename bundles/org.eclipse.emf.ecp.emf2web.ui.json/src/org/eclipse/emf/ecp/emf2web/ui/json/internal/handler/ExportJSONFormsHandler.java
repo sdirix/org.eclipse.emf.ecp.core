92c204ba1218f19e4e8731db5fd7e14110d6f3f0
Jonas Helming <jhelming@eclipsesource.com>
2017-04-05 08:43:16 +0200

    TCI - removed warning
    Removed unnecessary Supression
    
    Change-Id: I8aa436fac7c4d8cc387f9224270057e818b98033
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>

--- bundles/org.eclipse.emf.ecp.emf2web.ui.json/src/org/eclipse/emf/ecp/emf2web/ui/json/internal/handler/ExportJSONFormsHandler.java
+++ bundles/org.eclipse.emf.ecp.emf2web.ui.json/src/org/eclipse/emf/ecp/emf2web/ui/json/internal/handler/ExportJSONFormsHandler.java
@@ -44,7 +44,6 @@ import org.eclipse.ui.handlers.HandlerUtil;
 /**
  * The handler responsible for exporting view models to JSONForms.
  */
-@SuppressWarnings("restriction")
 public class ExportJSONFormsHandler extends AbstractSchemaExportCommandHandler {
 
 	private final List<String> registeredEcores = new LinkedList<String>();
