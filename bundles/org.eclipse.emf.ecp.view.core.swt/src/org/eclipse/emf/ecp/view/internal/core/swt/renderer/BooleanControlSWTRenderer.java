ecde3da2eb8fa7d7be7674b21f0bdae33155c475
Jonas Helming <jhelming@eclipsesource.com>
2017-05-11 15:46:13 +0200

    Bug 516507 - Boolean Renderer shall not fill horizontally
    Change-Id: I65c85dc519d9b95db33e696211f5c976f266f538
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>

--- bundles/org.eclipse.emf.ecp.view.core.swt/src/org/eclipse/emf/ecp/view/internal/core/swt/renderer/BooleanControlSWTRenderer.java
+++ bundles/org.eclipse.emf.ecp.view.core.swt/src/org/eclipse/emf/ecp/view/internal/core/swt/renderer/BooleanControlSWTRenderer.java
@@ -66,6 +66,7 @@ public class BooleanControlSWTRenderer extends SimpleControlSWTControlSWTRendere
 		// checkbox button representing Boolean value can not grow
 		final SWTGridCell booleanControlCell = super.createControlCell(column);
 		booleanControlCell.setHorizontalGrab(false);
+		booleanControlCell.setHorizontalFill(false);
 		return booleanControlCell;
 	}
 
