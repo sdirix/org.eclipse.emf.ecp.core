67b2cd2d1f73d4369f3d0f3fc888667020664dc5
Stefan Dirix <sdirix@eclipsesource.com>
2017-07-31 18:37:35 +0200

    Initial version of Json Graph Exporter

---
+++ bundles/org.eclipse.emf.ecp.emf2web.ui.graph.json/src/org/eclipse/emf/ecp/emf2web/graph/ui/json/internal/messages/Messages.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Stefan Dirix - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.ecp.emf2web.graph.ui.json.internal.messages;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Stefan Dirix
+ * @generated
+ */
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.emf.ecp.emf2web.ui.json.internal.handler.messages.messages"; //$NON-NLS-1$
+	public static String ExportJSONFormsHandler_ErrorResolvedViews;
+	public static String ExportJSONFormsHandler_ExportCanceled;
+	public static String ExportJSONFormsHandler_ViewsNotResolved;
+	public static String ExportJSONFormsHandler_ViewsSkipped;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
