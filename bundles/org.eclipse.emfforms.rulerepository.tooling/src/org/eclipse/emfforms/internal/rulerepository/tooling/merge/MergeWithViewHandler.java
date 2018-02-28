477dfd97fca6de17006e68e27767461a0438e79f
Lucas Koehler <lkoehler@eclipsesource.com>
2018-02-28 10:52:14 +0100

    Bug 531780 - Rule Repository Tooling - Merge Dialog Does Not Open
    * Added a check to the MergeWithViewHandler that read in resources have
    content before trying to access it
    * Refactored the MergeWithViewHandler handler into separate providers to
    allow unit testing
    * Added a unit test bundle for the rule repository tooling and 2 tests:
    * The first test tests that all providers with correct intermediate
    results are used.
    * The second one tests that reading in an empty resource or one with
    irrelevant content does not throw an exception
    
    Change-Id: I04f7e8ea2711d83bc106e504988623082112bed1
    Signed-off-by: Lucas Koehler <lkoehler@eclipsesource.com>

--- bundles/org.eclipse.emfforms.rulerepository.tooling/src/org/eclipse/emfforms/internal/rulerepository/tooling/merge/MergeWithViewHandler.java
+++ bundles/org.eclipse.emfforms.rulerepository.tooling/src/org/eclipse/emfforms/internal/rulerepository/tooling/merge/MergeWithViewHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011-2016 EclipseSource Muenchen GmbH and others.
+ * Copyright (c) 2011-2018 EclipseSource Muenchen GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -77,30 +77,142 @@ public class MergeWithViewHandler extends AbstractHandler {
 		final ResourceSet resourceSet = ResourceSetHelpers.loadResourceSetWithProxies(
 			URI.createPlatformResourceURI(selectedFile.getFullPath().toOSString(), false),
 			new BasicCommandStack());
-		VView view = null;
-		VRuleRepository ruleRepository = null;
-		for (final Resource resource : resourceSet.getResources()) {
-			final EObject eObject = resource.getContents().get(0);
-			if (VView.class.isInstance(eObject)) {
-				view = VView.class.cast(eObject);
-			}
-			if (VRuleRepository.class.isInstance(eObject)) {
-				ruleRepository = VRuleRepository.class.cast(eObject);
-			}
-		}
-		if (ruleRepository == null) {
+
+		// FIXME Improve with Java8: Use method pointers
+		return this.mergeRuleRepoWithView(activeShell, resourceSet,
+			new ViewAndRepositoryProvider() {
+				@Override
+				public ViewAndRepository getViewAndRepository(ResourceSet resourceSet) {
+					return extractViewAndRepository(resourceSet);
+				}
+			},
+			new MergedViewPathProvider() {
+				@Override
+				public String getPath(Shell activeShell, VView view) {
+					return getMergedViewPath(activeShell, view);
+				}
+			},
+			new OperationProvider() {
+				@Override
+				public WorkspaceModifyOperation getOperation(Shell activeShell, String mergedViewPath,
+					VView mergedView) {
+					return getWorkspaceOperation(activeShell, mergedViewPath, mergedView);
+				}
+			},
+			new MergeProvider() {
+				@Override
+				public VView merge(VRuleRepository repository, VView view) {
+					return mergeRuleRepository(repository, view);
+				}
+
+			});
+	}
+
+	/** Provides a method to get the save path of the merged view. */
+	public interface MergedViewPathProvider {
+		/**
+		 *
+		 * @param activeShell The Shell
+		 * @param view The base view
+		 * @return The save path of the merged view
+		 */
+		String getPath(Shell activeShell, VView view);
+	}
+
+	/** Provides a method to create the {@link WorkspaceModifyOperation}. */
+	public interface OperationProvider {
+		/**
+		 * Get the {@link WorkspaceModifyOperation} that saved the merged view.
+		 *
+		 * @param activeShell The Shell
+		 * @param mergedViewPath The save path
+		 * @param mergedView The merged view
+		 * @return The {@link WorkspaceModifyOperation}
+		 */
+		WorkspaceModifyOperation getOperation(Shell activeShell, String mergedViewPath, VView mergedView);
+	}
+
+	/** Provides a method to get the {@link VView} and {@link VRuleRepository} from a {@link ResourceSet}. */
+	public interface ViewAndRepositoryProvider {
+		/**
+		 *
+		 * @param resourceSet The ResourceSet
+		 * @return The RuleRepository and View
+		 */
+		ViewAndRepository getViewAndRepository(ResourceSet resourceSet);
+	}
+
+	/** Provides a method to merge the RuleRepository. */
+	public interface MergeProvider {
+		/**
+		 * Merges the RuleRepository in the View.
+		 *
+		 * @param repository The {@link VRuleRepository}
+		 * @param view The {@link VView}
+		 * @return The merged View
+		 */
+		VView merge(VRuleRepository repository, VView view);
+	}
+
+	/**
+	 * Merges a RuleRepository into its associated VView. Thereby, multiple providers are used to execute each step of
+	 * the algorithm.
+	 *
+	 * @param activeShell The {@link Shell}
+	 * @param resourceSet The {@link ResourceSet} containing the Rule Repository and the View
+	 * @param viewAndRepositoryProvider The {@link ViewAndRepositoryProvider}
+	 * @param pathProvider The {@link MergedViewPathProvider}
+	 * @param operationProvider The {@link OperationProvider}
+	 * @param mergeProvider The {@link MergeProvider}
+	 * @return The {@link WorkspaceModifyOperation} saving the merged View
+	 */
+	WorkspaceModifyOperation mergeRuleRepoWithView(final Shell activeShell, final ResourceSet resourceSet,
+		ViewAndRepositoryProvider viewAndRepositoryProvider, MergedViewPathProvider pathProvider,
+		OperationProvider operationProvider, MergeProvider mergeProvider) {
+
+		final ViewAndRepository vur = viewAndRepositoryProvider.getViewAndRepository(resourceSet);
+
+		if (vur.getRuleRepository() == null) {
 			ErrorDialog.openError(activeShell, "Missing Rule Repository", //$NON-NLS-1$
 				"The file doesn't contain a rule repository!", //$NON-NLS-1$
 				new Status(IStatus.ERROR, ORG_ECLIPSE_EMFFORMS_RULEREPOSITORY_TOOLING, "No Rule Repository!")); //$NON-NLS-1$
 			return null;
 		}
-		if (view == null) {
+		if (vur.getView() == null) {
 			ErrorDialog.openError(activeShell, "Missing View", //$NON-NLS-1$
 				"You must link a view model first!", //$NON-NLS-1$
 				new Status(IStatus.ERROR, ORG_ECLIPSE_EMFFORMS_RULEREPOSITORY_TOOLING, "No View model linked!")); //$NON-NLS-1$
 			return null;
 		}
 
+		final String mergedViewPath = pathProvider.getPath(activeShell, vur.getView());
+
+		final VView mergedView = mergeProvider.merge(vur.getRuleRepository(), vur.getView());
+
+		// Do the work within an operation.
+		return operationProvider.getOperation(activeShell, mergedViewPath, mergedView);
+	}
+
+	/**
+	 * Merges the given rule repository into its linked VView.
+	 *
+	 * @param repository The {@link VRuleRepository}
+	 * @param view The VView linked in the {@link VRuleRepository}
+	 * @return The merged {@link VView}
+	 */
+	VView mergeRuleRepository(VRuleRepository repository, VView view) {
+		MergeHelper.merge(repository);
+		return EcoreUtil.copy(view);
+	}
+
+	/**
+	 * Opens a file save dialog to determine the save path of the merged view.
+	 *
+	 * @param activeShell The active Shell
+	 * @param view The View object
+	 * @return The save path
+	 */
+	String getMergedViewPath(final Shell activeShell, VView view) {
 		final SaveAsDialog sad = new SaveAsDialog(activeShell);
 		final URI viewURI = view.eResource().getURI();
 		final IWorkspace workspace = ResourcesPlugin.getWorkspace();
@@ -114,17 +226,27 @@ public class MergeWithViewHandler extends AbstractHandler {
 		if (result == Window.CANCEL) {
 			return null;
 		}
-		MergeHelper.merge(ruleRepository);
-		final VView mergedView = EcoreUtil.copy(view);
-		// Do the work within an operation.
-		final WorkspaceModifyOperation operation = new WorkspaceModifyOperation() {
+		return sad.getResult().toString();
+	}
+
+	/**
+	 * Creates a {@link WorkspaceModifyOperation} to save the merged view model to the specified file.
+	 *
+	 * @param activeShell The active Shell
+	 * @param mergedViewPath The path where the merged view is saved
+	 * @param mergedView The merged view
+	 * @return The {@link WorkspaceModifyOperation}
+	 */
+	WorkspaceModifyOperation getWorkspaceOperation(final Shell activeShell, final String mergedViewPath,
+		final VView mergedView) {
+		return new WorkspaceModifyOperation() {
 			@Override
 			protected void execute(IProgressMonitor progressMonitor) {
 				// Create a resource set
 				final ResourceSet resourceSet = new ResourceSetImpl();
 
 				// Get the URI of the model file.
-				final URI fileURI = URI.createPlatformResourceURI(sad.getResult().toString(), true);
+				final URI fileURI = URI.createPlatformResourceURI(mergedViewPath, true);
 
 				// Create a resource for this file.
 				final Resource resource = resourceSet.createResource(fileURI);
@@ -146,7 +268,62 @@ public class MergeWithViewHandler extends AbstractHandler {
 				}
 			}
 		};
-		return operation;
 	}
 
+	/**
+	 * Gets the VView and VRuleRepository from the given {@link ResourceSet}.
+	 *
+	 * @param resourceSet The resource set containing the view and rule repository
+	 * @return The {@link ViewAndRepository} container
+	 */
+	ViewAndRepository extractViewAndRepository(ResourceSet resourceSet) {
+		final ViewAndRepository result = new ViewAndRepository();
+		for (final Resource resource : resourceSet.getResources()) {
+			if (resource.getContents().isEmpty()) {
+				continue;
+			}
+			final EObject eObject = resource.getContents().get(0);
+			if (VView.class.isInstance(eObject)) {
+				result.setView(VView.class.cast(eObject));
+			}
+			if (VRuleRepository.class.isInstance(eObject)) {
+				result.setRuleRepository(VRuleRepository.class.cast(eObject));
+			}
+		}
+		return result;
+	}
+
+	/** Wrapper class containing a VView and a VRuleRepository. */
+	public static class ViewAndRepository {
+		private VView view;
+		private VRuleRepository ruleRepository;
+
+		/**
+		 * @return the view
+		 */
+		public VView getView() {
+			return view;
+		}
+
+		/**
+		 * @param view the view to set
+		 */
+		public void setView(VView view) {
+			this.view = view;
+		}
+
+		/**
+		 * @return the ruleRepository
+		 */
+		public VRuleRepository getRuleRepository() {
+			return ruleRepository;
+		}
+
+		/**
+		 * @param ruleRepository the ruleRepository to set
+		 */
+		public void setRuleRepository(VRuleRepository ruleRepository) {
+			this.ruleRepository = ruleRepository;
+		}
+	}
 }
