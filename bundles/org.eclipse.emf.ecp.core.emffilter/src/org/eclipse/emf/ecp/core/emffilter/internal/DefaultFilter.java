f51d899895892a44b0bd86bea1ab6ddf190df65d
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2018-04-06 10:31:35 +0200

    Bug 533174, Bug 533201, Bug 533218 - Adapt DefaultFilter
    Change-Id: I2bee3d15b87166201ad3a15f92fad28923e060aa
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- bundles/org.eclipse.emf.ecp.core.emffilter/src/org/eclipse/emf/ecp/core/emffilter/internal/DefaultFilter.java
+++ bundles/org.eclipse.emf.ecp.core.emffilter/src/org/eclipse/emf/ecp/core/emffilter/internal/DefaultFilter.java
@@ -84,6 +84,7 @@ public class DefaultFilter implements ECPFilterProvider {
 		packages.add("http://org/eclipse/emf/ecp/view/separator/model"); //$NON-NLS-1$
 		packages.add("http://org/eclipse/emf/ecp/view/table/model/150"); //$NON-NLS-1$
 		packages.add("http://org/eclipse/emf/ecp/view/vertical/model"); //$NON-NLS-1$
+		packages.add("http://org/eclipse/emf/emfforms/view/annotation/model"); //$NON-NLS-1$
 
 		packages.add("http://org/eclipse/emf/ecp/view/categorization/model"); //$NON-NLS-1$
 		packages.add("http://org/eclipse/emf/ecp/view/template/model"); //$NON-NLS-1$
@@ -98,6 +99,9 @@ public class DefaultFilter implements ECPFilterProvider {
 		packages.add("http://www.eclipse.org/emf/ecp/view/template/style/tab/model"); //$NON-NLS-1$
 		packages.add("http://www.eclipse.org/emf/ecp/view/template/style/table/model"); //$NON-NLS-1$
 		packages.add("http://www.eclipse.org/emf/ecp/view/template/style/unsettable/model"); //$NON-NLS-1$
+		packages.add("http://www.eclipse.org/emf/ecp/view/template/selector/annotation/model"); //$NON-NLS-1$
+		packages.add("http://www.eclipse.org/emf/ecp/view/template/selector/hierarchy/model"); //$NON-NLS-1$
+		packages.add("http://www.eclipse.org/emf/ecp/view/template/selector/bool/model"); //$NON-NLS-1$
 
 		packages.add("http://org/eclipse/emf/ecp/view/treemasterview/model"); //$NON-NLS-1$
 
